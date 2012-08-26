24324160676b64e14b52ac9545fcf3f44169b91a
feesler <feesler@47cf728b-45b6-4a0a-a543-cdce7c6e2aab>
2012-08-26 13:59:22 +0000

    + Add fail() function into modules/createaccount.php
    git-svn-id: https://svn.code.sf.net/p/jezvemoney/code-0/trunk@21 47cf728b-45b6-4a0a-a543-cdce7c6e2aab

--- modules/createaccount.php
+++ modules/createaccount.php
@@ -1,6 +1,15 @@
 <?php
 
 require_once("../db.php");
+require_once("../common.php");
+
+
+function fail()
+{
+	header("Location: ../accounts.php?newacc=fail");
+	exit();
+}
+
 
 session_start();
 
@@ -11,18 +20,16 @@ $name = mysql_real_escape_string($_POST['accname']);
 $balance = floatval($_POST['accbalance']);
 $curr_id = intval($_POST['acccurr']);
 
-if ($name && $name != "" && $curr_id != 0)
-{
-	$query = "INSERT INTO accounts (`id`, `user_id`, `curr_id`, `balance`, `initbalance`, `name`) VALUES (NULL, '".$userid."', '".$curr_id."', '".$balance."', '".$balance."', '".$name."');";
-	$result = mysql_query($query, $dbcnx);
+if (!$name || $name == "" || $curr_id == 0)
+	fail();
 
-	if (!mysql_errno())
-	{
-		header("Location: ../accounts.php?newacc=ok");
-		exit();
-	}
-}
 
-header("Location: ../accounts.php?newacc=fail");
+$query = "INSERT INTO accounts (`id`, `user_id`, `curr_id`, `balance`, `initbalance`, `name`)";
+$query .= " VALUES (NULL, '".$userid."', '".$curr_id."', '".$balance."', '".$balance."', '".$name."');";
+$result = mysql_query($query, $dbcnx);
+if (mysql_errno())
+	fail();
+
+header("Location: ../accounts.php?newacc=ok");
 
 ?>
