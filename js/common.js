8eecfd33a5d3c041dc744b63dcfb85fa1c1b4238
feesler <feesler@47cf728b-45b6-4a0a-a543-cdce7c6e2aab>
2012-08-26 13:57:32 +0000

    + Add functions to common.js
    git-svn-id: https://svn.code.sf.net/p/jezvemoney/code-0/trunk@20 47cf728b-45b6-4a0a-a543-cdce7c6e2aab

--- js/common.js
+++ js/common.js
@@ -10,10 +10,125 @@ function ge(a)
 }
 
 
+// Fix string to correct float number format
+function fixFloat(str)
+{
+	if (typeof(str) == "string")
+		return str.replace(/,/g, '.');
+	else if (typeof(str) == "number")
+		return str;
+	else
+		return null;
+}
+
+
+// Check is specified string is number
 function isNum(val)
 {
 	if (val == 0)
 		return true;
 	else
 		return res = (val / val) ? true : false;
+}
+
+
+// Get key code from event
+function getCode(e)
+{
+	var KeyIdentifierMap =
+	{
+		End		: 35,
+		Home		: 36,
+		Left		: 37,
+		Right		: 39,
+		'U+00007F'	: 46		// Delete
+	};
+
+	var iCode;
+
+	if (!e)
+		e = event;
+
+	iCode = (e.keyCode || e.charCode);
+	if (!iCode && e.keyIdentifier && (e.keyIdentifier in KeyIdentifierMap))
+			iCode = KeyIdentifierMap[e.keyIdentifier];
+
+	return iCode;
+}
+
+
+// Check specified key code is digit or allowed keys
+function isDigit(iCode)
+{
+	return ((iCode >= 48 && iCode <= 57)		// Numbers
+		|| (iCode >= 35 && iCode <= 40)		// Arrows, Home, End
+		|| iCode == 44				// Delete
+		|| iCode == 8				// Backspace
+		|| iCode == 46				// Delete
+		|| iCode == 9				// Tab
+		);
+}
+
+
+// Return caret position in specified input control
+function getCaretPos(obj)
+{
+	obj.focus();
+
+	if (obj.selectionStart)			//Gecko
+		return obj.selectionStart;
+	else if (document.selection)		//IE
+	{
+		var sel = document.selection.createRange();
+		var clone = sel.duplicate();
+		sel.collapse(true);
+		clone.moveToElementText(obj);
+		clone.setEndPoint('EndToEnd', sel);
+		return clone.text.length;
+	}
+
+	return 0;
+}
+
+
+// Check specified key code is float dot ('.' or ',')
+function isDot(code)
+{
+	return (code == 44 || code == 46);
+}
+
+
+// Check specified string have float dot ('.' or ',')
+function haveDot(str)
+{
+	return (typeof(str) == "string" && (str.indexOf('.') != -1 || str.indexOf(',') != -1));
+}
+
+
+function onFieldKey(event, obj)
+{
+	var code, isDig, isDel;
+
+	if (!obj)
+		return false;
+
+	code = getCode(event);
+	isDig = isDigit(code);
+	isDel = (isDig && event.which == 0);
+
+	if (!isDig)
+		return false;
+
+	if (!isDel)
+	{
+	// exclude ',' or '.' at start
+		if ((obj.value == '' || (obj.value != '' && getCaretPos(obj) == 0)) && isDot(code))
+			return false;
+
+	// exclude ',' or '.' in one string
+		if (obj.value != '' && isDot(code) && haveDot(obj.value))
+			return false;
+	}
+
+	return true;
 }
