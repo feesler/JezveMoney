23824e09ecb0c5132badee6f2bd1f458dc12241a
feesler <feesler@47cf728b-45b6-4a0a-a543-cdce7c6e2aab>
2012-08-26 13:47:13 +0000

    * Fix spend transaction
    git-svn-id: https://svn.code.sf.net/p/jezvemoney/code-0/trunk@12 47cf728b-45b6-4a0a-a543-cdce7c6e2aab

--- modules/spend.php
+++ modules/spend.php
@@ -7,22 +7,26 @@ session_start();
 
 $userid = checkUser('../login.php');
 
-$acc_id = intval($_POST['acc_id']);
+$acc_id = intval($_POST['accid']);
 $value = floatval($_POST['amount']);
 $comment = mysql_real_escape_string($_POST['comm']);
 
 if ($acc_id != 0 && $value != 0.0)
 {
-	$query = "INSERT INTO transactions (`id`, `account_id`, `type`, `value`) VALUES (NULL, '".$acc_id."', '1', '".$balance."', '".$balance."', '".$name."');";
+	$query = "INSERT INTO transactions (`id`, `account_id`, `type`, `value`, `comment`) ".
+			"VALUES (NULL, '".$acc_id."', '1', '".$value."', '".$comment."');";
 	$result = mysql_query($query, $dbcnx);
 
+echo($query."\r\n");
+echo(mysql_errno().":".mysql_error()."\r\n");
+/*
 	if (!mysql_errno())
 	{
 		$query2 = "UPDATE accounts SET balance = balance - ".$value." WHERE id=".$acc_id.";";
 		$result2 = mysql_query($query2, $dbcnx);
-	}
+	}*/
 }
 
-header("Location: ../index.php");
+//header("Location: ../index.php");
 
 ?>
