929a42f0ce9d6b18d3faa354a52fb56573f1f930
feesler <feesler@47cf728b-45b6-4a0a-a543-cdce7c6e2aab>
2012-08-26 14:09:00 +0000

    * Make username bold
    git-svn-id: https://svn.code.sf.net/p/jezvemoney/code-0/trunk@24 47cf728b-45b6-4a0a-a543-cdce7c6e2aab

--- templates/userblock.php
+++ templates/userblock.php
@@ -1,7 +1,7 @@
 	<tr>
 	<td style="margin-top: 15px; margin-right: 30px; width: 100%; height: 30px;" align="right">
 	<form id="logoutfrm" name="logoutfrm" method="post" action="./modules/logout.php">
-	<span style="margin-right: 20px;"><?php	echo(getUserName($userid)); ?> logged in</span><input class="btn" type="submit" value="Logout">
+	<span style="margin-right: 20px;"><b><?php echo(getUserName($userid)); ?></b> logged in</span><input class="btn" type="submit" value="Logout">
 	</form>
 	</td>
 	</tr>
