9ff3a5c3662b30e7df2d9d5e44a73c486694db43
feesler <feesler@47cf728b-45b6-4a0a-a543-cdce7c6e2aab>
2012-11-15 22:25:15 +0000

    + Add support of income in currency different to currency of account
    git-svn-id: https://svn.code.sf.net/p/jezvemoney/code-0/trunk@49 47cf728b-45b6-4a0a-a543-cdce7c6e2aab

--- modules/income.php
+++ modules/income.php
@@ -17,19 +17,21 @@ $userid = checkUser('../login.php');
 
 $acc_id = intval($_POST['accid']);
 $amount = floatval($_POST['amount']);
+$receipt = floatval($_POST['receipt']);
+$transcurr = intval($_POST['transcurr']);
 $trdate = strtotime($_POST['date']);
 $comment = mysql_real_escape_string($_POST['comm']);
 
 if (!$acc_id != 0 || $amount == 0.0 || $trdate == -1)
 	fail();
 
-$query = "INSERT INTO transactions (`id`, `src_id`, `dest_id`, `type`, `amount`, `charge`, `date`, `comment`) ".
-			"VALUES (NULL, 0, '".$acc_id."', 2, '".$amount."', '".$amount."', ".$trdate.", '".$comment."');";
+$query = "INSERT INTO transactions (`id`, `src_id`, `dest_id`, `type`, `amount`, `charge`, `cur_id`, `date`, `comment`) ".
+			"VALUES (NULL, 0, '".$acc_id."', 2, '".$amount."', '".$receipt."', ".$transcurr.", ".$trdate.", '".$comment."');";
 $result = mysql_query($query, $dbcnx);
 if (mysql_errno())
 	fail();
 
-$query = "UPDATE accounts SET balance = balance + ".$amount." WHERE id=".$acc_id.";";
+$query = "UPDATE accounts SET balance = balance + ".$receipt." WHERE id=".$acc_id.";";
 $result = mysql_query($query, $dbcnx);
 if (mysql_errno())
 	fail();
