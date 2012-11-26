1d9f44511c3419752306693b68cf6442dc89690f
feesler <feesler@47cf728b-45b6-4a0a-a543-cdce7c6e2aab>
2012-11-26 09:57:50 +0000

    * Fix delete of all transactions of user
    git-svn-id: https://svn.code.sf.net/p/jezvemoney/code-0/trunk@99 47cf728b-45b6-4a0a-a543-cdce7c6e2aab

--- modules/resetaccounts.php
+++ modules/resetaccounts.php
@@ -16,14 +16,8 @@ if (!is_numeric($userid))
 	fail();
 
 // delete all transactions of user
-$resArr = $db->selectQ("id", "acounts", "user_id=".$userid);
-foreach($resArr as $row)
-{
-	$acc_id = $row["id"];
-
-	if (!$db->deleteQ("transactions", "src_id=".$acc_id." OR dest_id=".$acc_id))
-		fail();
-}
+if (!$db->deleteQ("transactions", "user_id=".$userid))
+	fail();
 
 // delete all accounts of user
 if (!$db->deleteQ("accounts", "user_id=".$userid))
