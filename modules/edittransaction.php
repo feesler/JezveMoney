f40acf014074220115d77f8b0c4d65dacec7b924
feesler <feesler@47cf728b-45b6-4a0a-a543-cdce7c6e2aab>
2012-11-26 09:27:43 +0000

    + Add cancelTransaction() function to improve update transaction process
    git-svn-id: https://svn.code.sf.net/p/jezvemoney/code-0/trunk@96 47cf728b-45b6-4a0a-a543-cdce7c6e2aab

--- modules/edittransaction.php
+++ modules/edittransaction.php
@@ -9,6 +9,84 @@ function fail()
 }
 
 
+// Cancel changes of transaction
+function cancelTransaction($trans_id)
+{
+	global $db;
+
+	// check transaction is exist
+	$transArr = $db->selectQ("*", "transactions", "id=".$trans_id);
+	if (count($transArr) != 1)
+		return FALSE;
+
+	$trans = $transArr[0];
+	$src_id = intval($trans["src_id"]);
+	$dest_id = intval($trans["dest_id"]);
+	$transType = intval($trans["type"]);
+	$transAmount = floatval($trans["amount"]);
+	$transCharge = floatval($trans["charge"]);
+
+	if ($transType == 1)		// spend
+	{
+		// check account is exist
+		$resArr = $db->selectQ("*", "accounts", "id=".$src_id);
+		if (count($resArr) != 1)
+			return FALSE;
+
+		$oldBalance = floatval($resArr[0]["balance"]);
+
+		// update balance of account
+		$newBalance = $oldBalance + $transCharge;
+		if (!$db->updateQ("accounts", array("balance"), array($newBalance), "id=".$src_id))
+			fail();
+	}
+	else if ($transType == 2)		// income
+	{
+		// check account is exist
+		$resArr = $db->selectQ("*", "accounts", "id=".$dest_id);
+		if (count($resArr) != 1)
+			return FALSE;
+
+		$oldBalance = floatval($resArr[0]["balance"]);
+
+		// update balance of account
+		$newBalance = $oldBalance - $transCharge;
+		if (!$db->updateQ("accounts", array("balance"), array($newBalance), "id=".$dest_id))
+			fail();
+	}
+	else if ($transType == 3)		// transfer
+	{
+		// check source account is exist
+		$resArr = $db->selectQ("*", "accounts", "id=".$src_id);
+		if (count($resArr) != 1)
+			return FALSE;
+
+		$oldSrcBalance = floatval($resArr[0]["balance"]);
+
+		// update balance of source account
+		$newBalance = $oldSrcBalance + $transAmount;
+		if (!$db->updateQ("accounts", array("balance"), array($newBalance), "id=".$src_id))
+			return FALSE;
+
+		// check destination account is exist
+		$resArr = $db->selectQ("*", "accounts", "id=".$src_id);
+		if (count($resArr) != 1)
+			return FALSE;
+
+		$oldDestBalance = floatval($resArr[0]["balance"]);
+
+		// update balance of destination account
+		$newBalance = $oldDestBalance - $transCharge;
+		if (!$db->updateQ("accounts", array("balance"), array($newBalance), "id=".$dest_id))
+			return FALSE;
+	}
+	else
+		return FALSE;
+
+	return TRUE;
+}
+
+
 session_start();
 
 $userid = checkUser("../login.php");
@@ -33,15 +111,32 @@ $transArr = $db->selectQ("*", "transactions", "id=".$trans_id);
 if (count($transArr) != 1)
 	fail();
 
+// cancel transaction
+if (!cancelTransaction($trans_id))
+	fail();
+/*
 // check transaction type is the same
 if (intval($transArr[0]["type"]) != $trans_type)
 	fail();
 
 $oldAmount = floatval($transArr[0]["amount"]);
 $oldCharge = floatval($transArr[0]["charge"]);
+*/
+
 
 if ($trans_type == 1)	// spend
 {
+	if (!$db->updateQ("transactions", array("src_id", "dest_id", "type", "amount", "charge", "curr_id", "date", "comment"),
+								array($src_id, 0, 1, $amount, $charge, $transcurr, $fdate, $comment), "id=".$trans_id))
+		fail();
+
+	$query = "UPDATE accounts SET balance = balance - ".$charge." WHERE id=".$src_id.";";
+	$result = $db->rawQ($query);
+	if (mysql_errno())
+		fail();
+
+	setLocation("../transactions.php?type=expense&edit=ok");
+/*
 	if (!$src_id)
 		fail();
 
@@ -60,9 +155,21 @@ if ($trans_type == 1)	// spend
 	$newBalance = $oldBalance + $oldCharge - $charge;
 	if (!$db->updateQ("accounts", array("balance"), array($newBalance), "id=".$src_id))
 		fail();
+*/
 }
 else if ($trans_type == 2)	// income
 {
+	if (!$db->updateQ("transactions", array("src_id", "dest_id", "type", "amount", "charge", "curr_id", "date", "comment"),
+								array(0, $dest_id, 2, $amount, $charge, $transcurr, $fdate, $comment), "id=".$trans_id))
+		fail();
+	
+	$query = "UPDATE accounts SET balance = balance + ".$charge." WHERE id=".$acc_id.";";
+	$result = $db->rawQ($query);
+	if (mysql_errno())
+		fail();
+
+	setLocation("../transactions.php?type=income&edit=ok");
+/*
 	if (!$dest_id)
 		fail();
 
@@ -82,9 +189,31 @@ else if ($trans_type == 2)	// income
 	$newBalance = $oldBalance - $oldCharge + $charge;
 	if (!$db->updateQ("accounts", array("balance"), array($newBalance), "id=".$src_id))
 		fail();
+*/
 }
 else if ($trans_type == 3)	// transfer
 {
+	$resArr = $db->selectQ("id", "accounts", "id=".$dest_id);
+	if (count($resArr) != 1)
+		fail();
+	$dest_curr_id = intval($resArr[0]["curr_id"]);		// currency of destination account is currency of transaction
+
+	if (!$db->updateQ("transactions", array("src_id", "dest_id", "type", "amount", "charge", "curr_id", "date", "comment"),
+								array($src_id, $dest_id, 3, $amount, $charge, $dest_curr_id, $fdate, $comment), "id=".$trans_id))
+		fail();
+
+	$query = "UPDATE accounts SET balance = balance - ".$amount." WHERE id=".$src_id.";";
+	$result = $db->rawQ($query, $dbcnx);
+	if (mysql_errno())
+		fail();
+
+	$query = "UPDATE accounts SET balance = balance + ".$charge." WHERE id=".$dest_id.";";
+	$result = $db->rawQ($query, $dbcnx);
+	if (mysql_errno())
+		fail();
+
+	setLocation("../transactions.php?type=transfer&edit=ok");
+/*
 	if (!$src_id || !$dest_id)
 		fail();
 
@@ -117,9 +246,9 @@ else if ($trans_type == 3)	// transfer
 	$newBalance = $oldDestBalance - $charge + $amount;
 	if (!$db->updateQ("accounts", array("balance"), array($newBalance), "id=".$src_id))
 		fail();
+*/
 }
-
-
-setLocation("../transactions.php?edit=ok");
+else
+	fail();
 
 ?>
