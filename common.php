87b652e3a8990263de9a68fcf79ae7a33da2511a
feesler <feesler@47cf728b-45b6-4a0a-a543-cdce7c6e2aab>
2012-11-28 14:38:19 +0000

    * Move cancelTransaction() function to common.php
    git-svn-id: https://svn.code.sf.net/p/jezvemoney/code-0/trunk@138 47cf728b-45b6-4a0a-a543-cdce7c6e2aab

--- common.php
+++ common.php
@@ -314,4 +314,76 @@ function getSign($accCurr, $curr_id)
 	return NULL;
 }
 
+
+// Cancel changes of transaction
+function cancelTransaction($trans_id)
+{
+	global $db;
+
+	// check transaction is exist
+	$transArr = $db->selectQ("*", "transactions", "id=".$trans_id);
+	if (count($transArr) != 1)
+		return FALSE;
+
+	$trans = $transArr[0];
+	$src_id = intval($trans["src_id"]);
+	$dest_id = intval($trans["dest_id"]);
+	$transType = intval($trans["type"]);
+	$transAmount = floatval($trans["amount"]);
+	$transCharge = floatval($trans["charge"]);
+
+	// check source account is exist
+	$srcBalance = 0;
+	if ($src_id != 0)
+	{
+		$resArr = $db->selectQ("*", "accounts", "id=".$src_id);
+		if (count($resArr) != 1)
+			return FALSE;
+	
+		$srcBalance = floatval($resArr[0]["balance"]);
+	}
+
+	// check destination account is exist
+	$destBalance = 0;
+	if ($dest_id != 0)
+	{
+		$resArr = $db->selectQ("*", "accounts", "id=".$dest_id);
+		if (count($resArr) != 1)
+			return FALSE;
+
+		$destBalance = floatval($resArr[0]["balance"]);
+	}
+
+	if ($transType == 1)		// spend
+	{
+		// update balance of account
+		$srcBalance += $transCharge;
+		if (!$db->updateQ("accounts", array("balance"), array($srcBalance), "id=".$src_id))
+			fail();
+	}
+	else if ($transType == 2)		// income
+	{
+		// update balance of account
+		$destBalance -= $transCharge;
+		if (!$db->updateQ("accounts", array("balance"), array($destBalance), "id=".$dest_id))
+			fail();
+	}
+	else if ($transType == 3)		// transfer
+	{
+		// update balance of source account
+		$srcBalance += $transCharge;
+		if (!$db->updateQ("accounts", array("balance"), array($srcBalance), "id=".$src_id))
+			return FALSE;
+
+		// update balance of destination account
+		$destBalance -= $transAmount;
+		if (!$db->updateQ("accounts", array("balance"), array($destBalance), "id=".$dest_id))
+			return FALSE;
+	}
+	else
+		return FALSE;
+
+	return TRUE;
+}
+
 ?>
