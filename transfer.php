ca19c9b7c95f8fb9029bb7a9945eb01986012e41
feesler <feesler@47cf728b-45b6-4a0a-a543-cdce7c6e2aab>
2012-11-28 11:39:16 +0000

    + Add onFInput() function to calculate transfer transaction parameters
    git-svn-id: https://svn.code.sf.net/p/jezvemoney/code-0/trunk@131 47cf728b-45b6-4a0a-a543-cdce7c6e2aab

--- transfer.php
+++ transfer.php
@@ -26,6 +26,136 @@
 	echo(getCurrencyArray());
 ?>
 
+var S1;		// balance before transaction
+var a;		// amount in currency of transaction
+var d;		// charge off in currency of account
+var e;		// exchange rate
+var S2;		// balance after transaction
+
+// Main formula
+// S2 = S1 - d
+// d = a * e
+
+
+function f1(){	S2 = S1 - d;	}
+function f2(){	d = a * e;		}
+function f3(){	d = S1 - S2;	}
+function f4(){	a = d / e;		}
+function f5(){	e = d / a;		}
+
+function getValues()
+{
+	var srcid, amount, charge, exchrate, resbal;
+
+	srcid = ge('srcid');
+	amount = ge('amount');
+	charge = ge('charge');
+	exchrate = ge('exchrate');
+	resbal = ge('resbal');
+	if (!srcid || !amount || !charge || !exchrate || !resbal)
+		return;
+
+	S1 = getBalanceOfAccount(selectedValue(srcid));
+	a = amount.value;
+	d = charge.value;
+	e = exchrate.value;
+	S2 = resbal.value;
+}
+
+
+function setValues()
+{
+	var amount, charge, exchrate, resbal;
+
+	amount = ge('amount');
+	charge = ge('charge');
+	exchrate = ge('exchrate');
+	resbal = ge('resbal');
+	if (!amount || !charge || !exchrate || !resbal)
+		return;
+
+	amount.value = a;
+	charge.value = d;
+	exchrate.value = e;
+	resbal.value = S2;
+}
+
+
+
+function onFInput(obj)
+{
+	var s1valid, s2valid, dvalid, evalid, avalid;
+
+	getValues();
+
+	s1valid = (S1 !== '');
+	s2valid = (S2 !== '');
+	dvalid = (d !== '');
+	evalid = (e !== '');
+	avalid = (a !== '');
+
+
+	if (s1valid && s2valid && dvalid && evalid && avalid)
+	{
+		if (obj.id == 'charge')		// d is changed, update S2 and e
+		{
+			f5();
+			f1();
+		}
+		else if (obj.id == 'resbal')	// S2 is changed, update d and e
+		{
+			f3();
+			f5();
+		}
+		else if (obj.id == 'amount' || obj.id == 'exchrate')	// a or e is changed, update S2 and d
+		{
+			f2();
+			f1();
+		}
+	}
+	else if (e == 1)		// account currency is the same as operation currency
+	{
+		d = a;
+		f1();
+	}
+	else				// account currency is different from operation currency
+	{
+		if (s1valid && evalid && s2valid)
+		{
+			f3();
+			f4();
+		}
+		else if (s1valid && evalid && avalid)
+		{
+			f2();
+			f1();
+		}
+		else if (s1valid && evalid && dvalid)
+		{
+			f1();
+			f4();
+		}
+		else if (s1valid && s2valid && avalid)
+		{
+			f3();
+			f5();
+		}
+		else if (s1valid && s2valid && dvalid && !evalid && !avalid)
+		{
+			return;
+		}
+		else if (s1valid && avalid && dvalid)
+		{
+			f1();
+			f5();
+		}
+	}
+
+	setValues();
+
+	return true;
+}
+
 </script>
 </head>
 <body>
@@ -79,7 +209,7 @@
 
 		<tr>
 		<td style="text-align: right;"><span style="margin-right: 5px;">Transfer amount</span></td>
-		<td><input class="inp" id="amount" name="amount" oninput="return onInput(this);" onkeypress="return onFieldKey(event, this);"><span id="amountsign" style="margin-left: 5px;"><?php echo(getCurSign($accCurr, $src_id)); ?></span></td>
+		<td><input class="inp" id="amount" name="amount" oninput="return onFInput(this);" onkeypress="return onFieldKey(event, this);"><span id="amountsign" style="margin-left: 5px;"><?php echo(getCurSign($accCurr, $src_id)); ?></span></td>
 		</tr>
 
 <?php
@@ -89,7 +219,7 @@
 		echo(">\r\n");
 ?>
 		<td style="text-align: right;"><span style="margin-right: 5px;">Charge off</span></td>
-		<td><input class="inp" id="charge" name="charge" oninput="return onInput(this);" onkeypress="return onFieldKey(event, this);"><span id="chargesign" style="margin-left: 5px;"><?php echo(getCurSign($accCurr, $dest_id)); ?></span></td>
+		<td><input class="inp" id="charge" name="charge" oninput="return onFInput(this);" onkeypress="return onFieldKey(event, this);"><span id="chargesign" style="margin-left: 5px;"><?php echo(getCurSign($accCurr, $dest_id)); ?></span></td>
 		</tr>
 
 <?php
@@ -99,7 +229,12 @@
 		echo(">\r\n");
 ?>
 		<td style="text-align: right;"><span style="margin-right: 5px;">Exchange rate</span></td>
-		<td><input class="inp" id="exchrate" name="exchrate" oninput="return onInput(this);" onkeypress="return onFieldKey(event, this);"></td>
+		<td><input class="inp" id="exchrate" name="exchrate" oninput="return onFInput(this);" onkeypress="return onFieldKey(event, this);"></td>
+		</tr>
+
+		<tr>
+		<td style="text-align: right;"><span style="margin-right: 5px;">Result balance</span></td>
+		<td><input class="inp" id="resbal" name="resbal" oninput="return onFInput(this);" onkeypress="return onFieldKey(event, this);"></td>
 		</tr>
 
 		<tr>
