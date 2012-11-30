de4813489b12f4b7a8cc48531e09bf0ba6e26884
feesler <feesler@47cf728b-45b6-4a0a-a543-cdce7c6e2aab>
2012-11-30 15:37:31 +0000

    + Add .userblock CSS class
    git-svn-id: https://svn.code.sf.net/p/jezvemoney/code-0/trunk@148 47cf728b-45b6-4a0a-a543-cdce7c6e2aab

--- templates/userblock.php
+++ templates/userblock.php
@@ -1,5 +1,5 @@
 	<tr>
-	<td style="margin-top: 15px; margin-right: 30px; width: 100%; height: 30px;" align="right">
+	<td class="userblock">
 	<form id="logoutfrm" name="logoutfrm" method="post" action="./modules/logout.php">
 	<span style="margin-right: 20px;"><b><?php echo(getUserName($userid)); ?></b> logged in</span><input class="btn" type="submit" value="Logout">
 	</form>
