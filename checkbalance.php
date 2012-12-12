8b5e179d3df1f68ea0b73c627f84f35421c6edc6
feesler <feesler@47cf728b-45b6-4a0a-a543-cdce7c6e2aab>
2012-12-12 10:42:54 +0000

    * Improve check balance page
    git-svn-id: https://svn.code.sf.net/p/jezvemoney/code-0/trunk@161 47cf728b-45b6-4a0a-a543-cdce7c6e2aab

--- checkbalance.php
+++ checkbalance.php
@@ -3,7 +3,7 @@
 
 	function ebr($str = "")
 	{
-		echo($str."<br>");
+		echo($str."\r\n");
 	}
 
 	function fail()
@@ -23,7 +23,15 @@
 	if (!$checkAccount_id)
 		fail();
 
-	ebr(getAccountName($checkAccount_id));
+	ebr("<!DOCTYPE HTML>");
+	ebr("<html>");
+	ebr("<head>");
+	ebr("<title>Check balance</title>");
+	ebr("</head>");
+	ebr("<body>");
+
+	ebr("<table>");
+	ebr("<tr><td colspan=\"3\">".getAccountName($checkAccount_id)."</td></tr>");
 
 	$resArr = $db->selectQ("*", "accounts", "id=".$checkAccount_id." AND user_id=".$userid);
 	if (count($resArr) != 1)
@@ -32,64 +40,73 @@
 	$initBalance = floatval($resArr[0]["initbalance"]);
 	$curBalance = floatval($resArr[0]["balance"]);
 
-	ebr("initBalance: ".$initBalance);
-	ebr("curBalance: ".$curBalance);
+	ebr("<tr><td colspan=\"3\">initBalance: ".$initBalance."</td></tr>");
+	ebr("<tr><td colspan=\"3\">curBalance: ".$curBalance."</td></tr>");
 	ebr();
 
 	$realBalance = $initBalance;
 
 	// incomes
 	ebr();
-	ebr("incomes");
+	ebr("<tr><td colspan=\"3\">incomes</td></tr>");
 	$resArr = $db->selectQ("*", "transactions", "dest_id=".$checkAccount_id." AND type=2 ORDER BY date ASC");
 	foreach($resArr as $row)
 	{
+		$amount = floatval($row["amount"]);
 		$charge = floatval($row["charge"]);
 		$comment = $row["comment"];
 
 		$realBalance += $charge;
-		ebr("+".$charge." ".$comment);
+		ebr("<tr><td>+".$charge.(($charge != $amount) ? " (".$amount.")" : "")."</td><td>".$comment."</td><td>".$realBalance."</td></tr>");
 	}
 
 	// expenses
 	ebr();
-	ebr("expenses");
+	ebr("<tr><td colspan=\"3\">expenses</td></tr>");
 	$resArr = $db->selectQ("*", "transactions", "src_id=".$checkAccount_id." AND type=1 ORDER BY date ASC");
 	foreach($resArr as $row)
 	{
+		$amount = floatval($row["amount"]);
 		$charge = floatval($row["charge"]);
 		$comment = $row["comment"];
 
 		$realBalance -= $charge;
-		ebr("-".$charge." ".$comment);
+		ebr("<tr><td>-".$charge.(($charge != $amount) ? " (".$amount.")" : "")."</td><td>".$comment."</td><td>".$realBalance."</td></tr>");
 	}
 
 	// transfers
 	ebr();
-	ebr("transfers from");
+	ebr("<tr><td colspan=\"3\">transfers from</td></tr>");
 	$resArr = $db->selectQ("*", "transactions", "src_id=".$checkAccount_id." AND type=3 ORDER BY date ASC");
 	foreach($resArr as $row)
 	{
+		$amount = floatval($row["amount"]);
 		$charge = floatval($row["charge"]);
 		$comment = $row["comment"];
 
 		$realBalance -= $charge;
-		ebr("-".$charge." ".$comment);
+		ebr("<tr><td>-".$charge.(($charge != $amount) ? " (".$amount.")" : "")."</td><td>".$comment."</td><td>".$realBalance."</td></tr>");
 	}
 
 	// transfers
 	ebr();
-	ebr("transfers to");
+	ebr("<tr><td colspan=\"3\">transfers to</td></tr>");
 	$resArr = $db->selectQ("*", "transactions", "dest_id=".$checkAccount_id." AND type=3 ORDER BY date ASC");
 	foreach($resArr as $row)
 	{
 		$amount = floatval($row["amount"]);
+		$charge = floatval($row["charge"]);
 		$comment = $row["comment"];
 
 		$realBalance += $amount;
-		ebr("+".$amount." ".$comment);
+		ebr("<tr><td>+".$amount.(($charge != $amount) ? " (".$charge.")" : "")."</td><td>".$comment."</td><td>".$realBalance."</td></tr>");
 	}
 
-	ebr();
-	ebr("realBalance: ".$realBalance);
+	ebr("<tr><td colspan=\"3\"></td></tr>");
+	ebr("<tr><td colspan=\"3\">realBalance: ".$realBalance."</td></tr>");
+	ebr("<tr><td colspan=\"3\">diference: ".($realBalance - $curBalance)."</td></tr>");
+	ebr("</table>");
+
+	ebr("</body>");
+	ebr("</html>");
 ?>
