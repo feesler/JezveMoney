8fc3e8011ad07cf2d78db6ba62e3d464bf5b38ec
feesler <feesler@47cf728b-45b6-4a0a-a543-cdce7c6e2aab>
2012-12-27 14:30:28 +0000

    + Add simple user profile page with password change feature
    git-svn-id: https://svn.code.sf.net/p/jezvemoney/code-0/trunk@205 47cf728b-45b6-4a0a-a543-cdce7c6e2aab

---
+++ modules/changepassword.php
@@ -0,0 +1,38 @@
+<?php
+	require_once("../setup.php");
+	require_once("../class/user.php");
+	require_once("../class/currency.php");
+	require_once("../class/account.php");
+
+
+	function fail()
+	{
+		setLocation("../profile.php?act=fail");
+		exit();
+	}
+
+	session_start();
+
+	$userid = checkUser("../login.php");
+
+
+	if (!isset($_POST["oldpwd"]) || $_POST["oldpwd"] == "" || !isset($_POST["newpwd"]) || $_POST["newpwd"] == "")
+		fail();
+
+	$login = getUserName($userid);
+	$oldpwd = $_POST["oldpwd"];
+	$newpwd = $_POST["newpwd"];
+
+	if (!checkLoginData($login, $oldpwd))
+		fail();
+
+	$passhash = createUserHash($login, $newpwd);
+	if (!$db->updateQ("users", array("passhash"), array($passhash), "id=".$userid))
+		fail();
+
+	$preHash = createPreHash($login, $newpwd);
+	setupCookies($login, $preHash);
+
+	setLocation("../profile.php?act=ok");
+
+?>
