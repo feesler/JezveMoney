480a05dd70b509c7495a99d61664910290adbe81
feesler <feesler@47cf728b-45b6-4a0a-a543-cdce7c6e2aab>
2013-01-10 12:14:34 +0000

    * Fix mistake in delete transaction module
    git-svn-id: https://svn.code.sf.net/p/jezvemoney/code-0/trunk@213 47cf728b-45b6-4a0a-a543-cdce7c6e2aab

--- modules/deltransaction.php
+++ modules/deltransaction.php
@@ -4,6 +4,7 @@
 	require_once("../class/user.php");
 	require_once("../class/currency.php");
 	require_once("../class/account.php");
+	require_once("../class/transaction.php");
 
 	function fail()
 	{
