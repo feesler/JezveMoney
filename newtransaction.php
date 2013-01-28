2a598878a155f29a39fd671c34840219dd584b79
feesler <feesler@47cf728b-45b6-4a0a-a543-cdce7c6e2aab>
2013-01-28 08:17:50 +0000

    * Idle
    git-svn-id: https://svn.code.sf.net/p/jezvemoney/code-0/trunk@253 47cf728b-45b6-4a0a-a543-cdce7c6e2aab

--- newtransaction.php
+++ newtransaction.php
@@ -104,109 +104,108 @@
 	$accounts = $acc->getCount();
 	if ($accounts > 0)
 	{
+		setTab(1);
 
-	setTab(1);
-
-	html("<tr>");
-	html("<td>");
-	html("<form method=\"post\" action=\"./modules/transaction.php?type=".$type_str."\" onsubmit=\"return ".(($trans_type == 3) ? "onTransferSubmit" : "onSubmit")."(this);\">");
-	html("<table>");
-
-	setTab(2);
-
-	if ($trans_type == 1 || $trans_type == 3)
-	{
 		html("<tr>");
-		html("<td class=\"lblcell\"><span>".$srcLbl."</span></td>");
 		html("<td>");
+		html("<form method=\"post\" action=\"./modules/transaction.php?type=".$type_str."\" onsubmit=\"return ".(($trans_type == 3) ? "onTransferSubmit" : "onSubmit")."(this);\">");
+		html("<table>");
 
-		setTab(3);
-		html("<select id=\"srcid\" name=\"srcid\" onchange=\"".(($trans_type == 3) ? "onChangeSource" : "onChangeAcc")."();\">");
-		echo($acc->getList($src_id));
-		html("</select>");
 		setTab(2);
 
+		if ($trans_type == 1 || $trans_type == 3)
+		{
+			html("<tr>");
+			html("<td class=\"lblcell\"><span>".$srcLbl."</span></td>");
+			html("<td>");
+
+			setTab(3);
+			html("<select id=\"srcid\" name=\"srcid\" onchange=\"".(($trans_type == 3) ? "onChangeSource" : "onChangeAcc")."();\">");
+			echo($acc->getList($src_id));
+			html("</select>");
+			setTab(2);
+
+			html("</td>");
+			html("</tr>");
+			html();
+		}
+
+		if ($trans_type == 2 || $trans_type == 3)
+		{
+			html("<tr>");
+			html("<td class=\"lblcell\"><span>".$destLbl."</span></td>");
+			html("<td>");
+			setTab(3);
+			html("<select id=\"destid\" name=\"destid\" onchange=\"".(($trans_type == 3) ? "onChangeDest" : "onChangeAcc")."();\">");
+			echo($acc->getList($dest_id));
+			html("</select>");
+			setTab(2);
+			html("</td>");
+			html("</tr>");
+			html();
+		}
+
+		html("<tr>");
+		html("<td class=\"lblcell\"><span>".$amountLbl."</span></td>");
+		echo($tabStr."<td><input id=\"amount\" name=\"amount\" type=\"text\" oninput=\"return onFInput(this);\" onkeypress=\"return onFieldKey(event, this);\">");
+		echo("<span id=\"amountsign\" class=\"currsign\">".(($trans_type == 1) ? $src_sign : $dest_sign)."</span>");
+		if ($trans_type != 3)
+		{
+			echo("<input id=\"ancurrbtn\" class=\"btn\" type=\"button\" onclick=\"showCurrList();\" value=\"currency\">");
+			html();
+			setTab(3);
+			html("<select id=\"transcurr\" name=\"transcurr\" style=\"display: none;\" onchange=\"onChangeTransCurr();\">");
+			echo(Currency::getList($src_curr));
+			html("</select>");
+			setTab(2);
+		}
 		html("</td>");
 		html("</tr>");
 		html();
-	}
 
-	if ($trans_type == 2 || $trans_type == 3)
-	{
-		html("<tr>");
-		html("<td class=\"lblcell\"><span>".$destLbl."</span></td>");
-		html("<td>");
-		setTab(3);
-		html("<select id=\"destid\" name=\"destid\" onchange=\"".(($trans_type == 3) ? "onChangeDest" : "onChangeAcc")."();\">");
-		echo($acc->getList($dest_id));
-		html("</select>");
-		setTab(2);
-		html("</td>");
+		$disp = (($trans_type != 3 || ($trans_type == 3 && $src_curr == $dest_curr)) ? " style=\"display: none;\"" : "");
+		html("<tr id=\"chargeoff\"".$disp.">");
+		html("<td class=\"lblcell\"><span>".$chargeLbl."</span></td>");
+		html("<td><input id=\"charge\" name=\"charge\" type=\"text\" oninput=\"return onFInput(this);\" onkeypress=\"return onFieldKey(event, this);\"><span id=\"chargesign\" class=\"currsign\">".$src_sign."</span></td>");
 		html("</tr>");
 		html();
-	}
 
-	html("<tr>");
-	html("<td class=\"lblcell\"><span>".$amountLbl."</span></td>");
-	echo($tabStr."<td><input id=\"amount\" name=\"amount\" type=\"text\" oninput=\"return onFInput(this);\" onkeypress=\"return onFieldKey(event, this);\">");
-	echo("<span id=\"amountsign\" class=\"currsign\">".(($trans_type == 1) ? $src_sign : $dest_sign)."</span>");
-	if ($trans_type != 3)
-	{
-		echo("<input id=\"ancurrbtn\" class=\"btn\" type=\"button\" onclick=\"showCurrList();\" value=\"currency\">");
+		$disp = (($trans_type != 3 || ($trans_type == 3 && $src_curr == $dest_curr)) ? " style=\"display: none;\"" : "");
+		html("<tr id=\"exchange\"".$disp.">");
+		html("<td class=\"lblcell\"><span>Exchange rate</span></td>");
+		html("<td><input id=\"exchrate\" name=\"exchrate\" type=\"text\" oninput=\"return onFInput(this);\" onkeypress=\"return onFieldKey(event, this);\" value=\"1\"><span id=\"exchcomm\" style=\"margin-left: 5px;\"></span></td>");
+		html("</tr>");
 		html();
-		setTab(3);
-		html("<select id=\"transcurr\" name=\"transcurr\" style=\"display: none;\" onchange=\"onChangeTransCurr();\">");
-		echo(Currency::getList($src_curr));
-		html("</select>");
-		setTab(2);
-	}
-	html("</td>");
-	html("</tr>");
-	html();
 
-	$disp = (($trans_type != 3 || ($trans_type == 3 && $src_curr == $dest_curr)) ? " style=\"display: none;\"" : "");
-	html("<tr id=\"chargeoff\"".$disp.">");
-	html("<td class=\"lblcell\"><span>".$chargeLbl."</span></td>");
-	html("<td><input id=\"charge\" name=\"charge\" type=\"text\" oninput=\"return onFInput(this);\" onkeypress=\"return onFieldKey(event, this);\"><span id=\"chargesign\" class=\"currsign\">".$src_sign."</span></td>");
-	html("</tr>");
-	html();
+		html("<tr>");
+		html("<td class=\"lblcell\"><span>Result balance</span></td>");
+		html("<td><input id=\"resbal\" name=\"resbal\" type=\"text\" oninput=\"return onFInput(this);\" onkeypress=\"return onFieldKey(event, this);\"></td>");
+		html("</tr>");
+		html();
 
-	$disp = (($trans_type != 3 || ($trans_type == 3 && $src_curr == $dest_curr)) ? " style=\"display: none;\"" : "");
-	html("<tr id=\"exchange\"".$disp.">");
-	html("<td class=\"lblcell\"><span>Exchange rate</span></td>");
-	html("<td><input id=\"exchrate\" name=\"exchrate\" type=\"text\" oninput=\"return onFInput(this);\" onkeypress=\"return onFieldKey(event, this);\" value=\"1\"><span id=\"exchcomm\" style=\"margin-left: 5px;\"></span></td>");
-	html("</tr>");
-	html();
+		html("<tr>");
+		html("<td class=\"lblcell\"><span>Date</span></td>");
+		html("<td><input id=\"date\" name=\"date\" type=\"text\" value=\"".date("d.m.Y")."\"></td>");
+		html("</tr>");
+		html();
 
-	html("<tr>");
-	html("<td class=\"lblcell\"><span>Result balance</span></td>");
-	html("<td><input id=\"resbal\" name=\"resbal\" type=\"text\" oninput=\"return onFInput(this);\" onkeypress=\"return onFieldKey(event, this);\"></td>");
-	html("</tr>");
-	html();
+		html("<tr>");
+		html("<td class=\"lblcell\"><span>Comment</span></td>");
+		html("<td><input id=\"comm\" name=\"comm\" type=\"text\"></td>");
+		html("</tr>");
+		html();
 
-	html("<tr>");
-	html("<td class=\"lblcell\"><span>Date</span></td>");
-	html("<td><input id=\"date\" name=\"date\" type=\"text\" value=\"".date("d.m.Y")."\"></td>");
-	html("</tr>");
-	html();
+		html("<tr>");
+		html("<td colspan=\"2\" style=\"text-align: center;\"><input type=\"submit\" value=\"ok\"></td>");
+		html("</tr>");
+		setTab(1);
 
-	html("<tr>");
-	html("<td class=\"lblcell\"><span>Comment</span></td>");
-	html("<td><input id=\"comm\" name=\"comm\" type=\"text\"></td>");
-	html("</tr>");
-	html();
+		html("</table>");
+		html("</form>");
+		html("</td>");
+		html("</tr>");
 
-	html("<tr>");
-	html("<td colspan=\"2\" style=\"text-align: center;\"><input type=\"submit\" value=\"ok\"></td>");
-	html("</tr>");
-	setTab(1);
-
-	html("</table>");
-	html("</form>");
-	html("</td>");
-	html("</tr>");
-
-	setTab(0);
+		setTab(0);
 	}
 
 	html("</table>");
