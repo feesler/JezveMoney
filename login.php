6bfd86c50fb73e91184750f30780b16b90c54ef8
feesler <feesler@47cf728b-45b6-4a0a-a543-cdce7c6e2aab>
2013-04-22 21:49:51 +0000

    * Move login page styles to login.css
    git-svn-id: https://svn.code.sf.net/p/jezvemoney/code-0/branches/design@344 47cf728b-45b6-4a0a-a543-cdce7c6e2aab

--- login.php
+++ login.php
@@ -21,97 +21,10 @@
 <?php
 	html("<title>".$titleString."</title>");
 	html(getCSS("common.css"));
-	html(getCSS("tiles.css"));
-	html(getCSS("table.css"));
+	html(getCSS("login.css"));
 	html(getJS("common.js"));
 	html(getJS("main.js"));
 ?>
-<style>
-/* layered layout */
-.cont
-{
-	position: absolute;
-	top: 50%;
-	left: 0%;
-	margin-top: -77px;
-	width: 100%;
-}
-
-
-.box
-{
-	max-width: 600px;
-	min-width: 130px;
-	margin: 0 auto;
-}
-
-
-.box > label
-{
-	width: 150px;
-	height: 20px;
-	line-height: 20px;
-	padding: 0px 5px;
-}
-
-
-.rdiv
-{
-	width: 100%;
-	min-width: 240px;
-	height: 50px;
-}
-
-
-.rdiv > div
-{
-	height: 32px;
-	padding: 8px 19px 10px 5px;
-}
-
-
-.rdiv > div > input
-{
-	width: 100%;
-	height: 26px;
-	margin: 0;
-}
-
-
-.box > div > .ok_btn
-{
-	font-family: "Segoe UI", "Lucida Sans Unicode", "Geneva", "Helvetica Neue", "Arial", "Nimbus Sans L", sans-serif;
-	margin: 29px 0px 8px 5px;
-	padding: 4px 22px;
-	border: 0 none;
-	background-color: #0072C6;
-	color: #FFFFFF;
-}
-
-
-.box > div > .ok_btn:hover
-{
-	background-color: #1E82CC;
-}
-
-
-.register_link
-{
-	margin: 0px 20px;
-}
-
-
-.register_link > a,
-.register_link > a:visited
-{
-	color: #0072C6;
-}
-
-.register_link > a:hover
-{
-	color: #1E82CC;
-}
-</style>
 </head>
 <body>
 <?php
