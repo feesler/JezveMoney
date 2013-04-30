633d23cd37592cb0889221f80fefe2dfbfb4022b
feesler <feesler@47cf728b-45b6-4a0a-a543-cdce7c6e2aab>
2013-04-30 13:01:24 +0000

    * Fix scroll issue of centered layout (log in, registration pages)
    git-svn-id: https://svn.code.sf.net/p/jezvemoney/code-0/branches/design@382 47cf728b-45b6-4a0a-a543-cdce7c6e2aab

--- login.php
+++ login.php
@@ -29,14 +29,16 @@
 	require_once("./templates/header.php");
 ?>
 <form action="./modules/login.php" method="post" onsubmit="return onLoginSubmit(this);">
-<div class="cont login_cont">
-	<div class="box">
-		<h1>Log in</h1>
-		<label for="login">Username</label>
-		<div class="rdiv"><div><input id="login" name="login" type="text"></div></div>
-		<label for="password">Password</label>
-		<div class="rdiv"><div><input id="password" name="password" type="password"></div></div>
-		<div><input class="ok_btn" type="submit" value="Log in"><span class="alter_link"><a href="./register.php">Register</a></span></div>
+<div class="page login_page">
+	<div class="cont">
+		<div class="box">
+			<h1>Log in</h1>
+			<label for="login">Username</label>
+			<div class="rdiv"><div><input id="login" name="login" type="text"></div></div>
+			<label for="password">Password</label>
+			<div class="rdiv"><div><input id="password" name="password" type="password"></div></div>
+			<div><input class="ok_btn" type="submit" value="Log in"><span class="alter_link"><a href="./register.php">Register</a></span></div>
+		</div>
 	</div>
 </div>
 </form>
--- register.php
+++ register.php
@@ -29,16 +29,18 @@
 	require_once("./templates/header.php");
 ?>
 <form action="./modules/register.php" method="post" onsubmit="return onLoginSubmit(this);">
-<div class="cont register_cont">
-	<div class="box">
-		<h1>Registration</h1>
-		<label for="login">Account name</label>
-		<div class="rdiv"><div><input id="login" name="login" type="text"></div></div>
-		<label for="login">Name</label>
-		<div class="rdiv"><div><input id="name" name="name" type="text"></div></div>
-		<label for="password">Password</label>
-		<div class="rdiv"><div><input id="password" name="password" type="password"></div></div>
-		<div><input class="ok_btn" type="submit" value="ok"><span class="alter_link"><a href="./login.php">Log in</a></span></div>
+<div class="page register_page">
+	<div class="cont">
+		<div class="box">
+			<h1>Registration</h1>
+			<label for="login">Account name</label>
+			<div class="rdiv"><div><input id="login" name="login" type="text"></div></div>
+			<label for="login">Name</label>
+			<div class="rdiv"><div><input id="name" name="name" type="text"></div></div>
+			<label for="password">Password</label>
+			<div class="rdiv"><div><input id="password" name="password" type="password"></div></div>
+			<div><input class="ok_btn" type="submit" value="ok"><span class="alter_link"><a href="./login.php">Log in</a></span></div>
+		</div>
 	</div>
 </div>
 </form>
