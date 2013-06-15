57010167fb99dd618dc4a6a0851cc9e7b21b8f0b
Ilya Makarov <feesler@gmail.com>
2013-06-15 23:54:25 +0400

    + [JS] Add Popup object constructor

--- js/main.js
+++ js/main.js
@@ -324,9 +324,30 @@ function onNewAccountSubmit(frm)
 }
 
 
+var dwPopup;
+
+
 // Create and show delete warning popup box
 function showPopupBox()
 {
+	dwPopup = new Popup();
+	if (!dwPopup)
+		return;
+
+	if (!dwPopup.create({ id : 'delete_warning',
+						title : 'Delete account',
+						msg : 'Are you sure want to delete selected account?<br>All income and expense transactions history will be lost. Transfer to this account will be changed to expense. Transfer from this account will be changed to income.',
+						btn : { okBtn : { type : 'submit' }, cancelBtn : { } }
+						}))
+	{
+		dwPopup = null;
+		return;
+	}
+
+	dwPopup.show();
+
+	
+/*
 	var popup;
 
 	popup = createPopup('delete_warning', 'Delete account', 'Are you sure want to delete selected account?<br>All income and expense transactions history will be lost. Transfer to this account will be changed to expense. Transfer from this account will be changed to income.');
@@ -342,9 +363,11 @@ function showPopupBox()
 	document.body.style.overflow = 'hidden';
 	document.documentElement.scrollTop = 0;
 	show(popup, true);
+*/
 }
 
 
+/*
 // Close delete warning popup box
 function hidePopupBox()
 {
@@ -358,6 +381,7 @@ function hidePopupBox()
 	document.body.style.overflow = '';
 	popup.parentNode.removeChild(popup);
 }
+*/
 
 
 // Create popup DOM structure and return object
@@ -384,4 +408,137 @@ function createPopup(p_id, title, msg)
 					]);
 
 	return popup;
+}
+
+
+// Popup constructor
+function Popup()
+{
+	this.popupObj = null;
+	this.backObj = null;
+	this.contentObj = null;
+	this.boxObj = null;
+	this.titleObj = null;
+	this.messageObj = null;
+	this.controlsObj = null;
+	this.okBtn = null;
+	this.cancelBtn = null;
+
+
+	this.mergeDef = function(obj, mergeObj)
+	{
+		var par, val;
+	
+		if (!obj || typeof obj !== 'object' || !mergeObj || typeof mergeObj !== 'object')
+			return;
+
+		for(par in mergeObj)
+		{
+			val = mergeObj[par];
+			if (typeof val === 'object')
+			{
+				if (!(par in obj))
+					obj[par] = {};
+				this.mergeDef(obj[par], val);
+			}
+			else if (!(par in obj))
+				obj[par] = val;
+		}
+	}
+
+
+	this.create = function(params)
+	{
+		var popupObj, backObj, contentObj, boxObj, titleObj, messageObj, controlsObj, okBtn, cancelBtn;
+
+		if (!params || !params.id || !params.title || !params.msg || !params.btn)
+			return false;
+
+		popupObj = ce('div', { id : params.id, className : 'popup', style : { display : 'none' } });
+		if (!popupObj)
+			return false;
+
+		backObj = ce('div', { className : 'popup_back' });
+		contentObj = ce('div', { className : 'popup_content' });
+		boxObj = ce('div', { className : 'box' });
+		if (!backObj || !contentObj || !boxObj)
+			return false;
+
+		titleObj = ce('h1', { className : 'popup_title', innerHTML : params.title });
+		messageObj = ce('div', { className : 'popup_message' }, [ ce('div', { innerHTML : params.msg }) ]);
+		controlsObj = ce('div', { className : 'popup_controls' });
+		if (!titleObj || !messageObj || !controlsObj)
+			return false;
+
+		this.popupObj = popupObj;
+		this.backObj = backObj;
+		this.contentObj = contentObj;
+		this.boxObj = boxObj;
+		this.titleObj = titleObj;
+		this.messageObj = messageObj;
+		this.controlsObj = controlsObj;
+
+		if (params.btn.okBtn)
+		{
+			okBtn = ce('input', { className : 'btn ok_btn' });
+			if (!okBtn)
+				return false;
+
+			this.mergeDef(params.btn.okBtn, { type : 'button', value : 'ok' });
+			setParam(okBtn, params.btn.okBtn);
+			this.okBtn = okBtn;
+		}
+
+		if (params.btn.cancelBtn)
+		{
+			cancelBtn = ce('input', { className : 'btn cancel_btn' });
+			if (!cancelBtn)
+				return false;
+
+			this.mergeDef(params.btn.cancelBtn, { type : 'button', value : 'cancel', onclick : bind(this.close, this) });
+			setParam(cancelBtn, params.btn.cancelBtn);
+			this.cancelBtn = cancelBtn;
+		}
+
+		addChilds(this.controlsObj, [this.okBtn, this.cancelBtn]);
+		addChilds(this.boxObj, [this.titleObj, this.messageObj, this.controlsObj]);
+		addChilds(this.contentObj, [this.boxObj]);
+		addChilds(this.popupObj, [this.backObj, this.contentObj]);
+
+		return true;
+	},
+
+
+	this.show = function()
+	{
+		if (!this.popupObj)
+			return;
+
+		document.body.appendChild(this.popupObj);
+		document.body.style.overflow = 'hidden';
+		document.documentElement.scrollTop = 0;
+		show(this.popupObj, true);
+	},
+
+
+	this.hide = function()
+	{
+		if (!this.popupObj)
+			return;
+
+		show(this.popupObj, false);
+		document.body.style.overflow = '';
+	},
+
+
+	this.close = function()
+	{
+		if (!this.popupObj)
+			return;
+
+		this.hide();
+
+		this.popupObj.parentNode.removeChild(this.popupObj);
+		this.popupObj = null;
+	}
 }
