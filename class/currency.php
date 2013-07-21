04020aa6fee597c76832fe1a528890d2c9e3f33c
Ilya Makarov <feesler@gmail.com>
2013-07-21 05:47:43 +0400

    * Merge currency changes from master

--- class/currency.php
+++ class/currency.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 
 class Currency
 {
@@ -76,6 +76,96 @@ class Currency
 	}
 
 
+	// Create new currency and return id if successfully
+	public static function create($curr_name, $curr_sign, $curr_format)
+	{
+		global $db;
+
+		$curr_name = $db->escape($curr_name);
+		$curr_sign = $db->escape($curr_sign);
+		$curr_format = intval($curr_format);
+
+		if (!$curr_name || $curr_name == "" || !$curr_sign || $curr_sign == "")
+			return 0;
+
+		if (!$db->insertQ("currency", array("id", "name", "sign", "format"),
+							array(NULL, $curr_name, $curr_sign, $curr_format)))
+			return 0;
+
+		self::updateCache();
+
+		return $db->insertId();
+	}
+
+
+	// Edit specified currency
+	public static function edit($curr_id, $curr_name, $curr_sign, $curr_format)
+	{
+		global $db;
+
+		$curr_id = intval($curr_id);
+		$curr_name = $db->escape($curr_name);
+		$curr_sign = $db->escape($curr_sign);
+		$curr_format = intval($curr_format);
+
+		if (!$curr_id || !$curr_name || $curr_name == "" || !$curr_sign || $curr_sign == "")
+			return FALSE;
+
+		if (!self::is_exist($curr_id))
+			return FALSE;
+
+		if (!$db->updateQ("currency", array("name", "sign", "format"),
+								array($curr_name, $curr_sign, $curr_format),
+								"id=".$curr_id))
+			return FALSE;
+
+		self::updateCache();
+
+		return TRUE;
+	}
+
+
+	// Check currency is in use
+	public static function isInUse($curr_id)
+	{
+		global $db;
+
+		$curr_id = intval($curr_id);
+		if (!$curr_id)
+			return FALSE;
+
+		$resArr = $db->selectQ("id", "account", "curr_id=".$curr_id);
+		if (count($resArr) > 0)
+			return TRUE;
+
+		$resArr = $db->selectQ("id", "transactions", "curr_id=".$curr_id);
+		if (count($resArr) > 0)
+			return TRUE;
+
+		return FALSE;
+	}
+
+
+	// Delete specified currency
+	public static function del($curr_id)
+	{
+		global $db;
+
+		$curr_id = intval($curr_id);
+		if (!$curr_id)
+			return FALSE;
+
+		// don't delete currencies in use
+		if (self::isInUse($curr_id))
+			return FALSE;
+
+		if (!$db->deleteQ("currency", "id=".$curr_id))
+			return FALSE;
+
+		return TRUE;
+	}
+
+
 	// Return name of specified currency
 	public static function getName($curr_id)
 	{
@@ -101,8 +191,10 @@ class Currency
 	public static function format($value, $curr_id)
 	{
 		$fmt = self::getFormat($curr_id);
+		$sign = self::getSign($curr_id);
 
-		return valFormat((is_null($fmt) ? "" : $fmt), $value);
+		$sfmt = (($fmt) ? ($sign." %s") : ("%s ".$sign));
+		return valFormat($sfmt, $value);
 	}
 
 
@@ -129,7 +221,7 @@ class Currency
 
 
 	// Return Javascript array of currencies
-	public static function getArray()
+	public static function getArray($ext = FALSE)
 	{
 		if (!self::checkCache())
 			return "";
@@ -138,7 +230,10 @@ class Currency
 
 		foreach(self::$cache as $curr_id => $row)
 		{
-			$resArr[] = array($curr_id, $row["name"], $row["sign"]);
+			if ($ext)
+				$resArr[] = array($curr_id, $row["name"], $row["sign"], $row["format"]);
+			else
+				$resArr[] = array($curr_id, $row["name"], $row["sign"]);
 		}
 
 		return "var currency = ".json_encode($resArr).";\r\n";
