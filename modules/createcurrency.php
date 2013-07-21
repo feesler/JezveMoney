9b9fd229bcb8dc732ab98c27b3ffc8ee24201022
Ilya Makarov <feesler@gmail.com>
2013-07-21 05:48:36 +0400

    * Merge currency modules from master

---
+++ modules/createcurrency.php
@@ -0,0 +1,26 @@
+<?php
+	require_once("../setup.php");
+	require_once("../class/user.php");
+	require_once("../class/currency.php");
+
+
+	function fail()
+	{
+		setLocation("../admin/currency.php?add=fail");
+	}
+
+
+	$user_id = User::check();
+	if (!$user_id)
+		setLocation("../login.php");
+
+	if (!isset($_POST["curr_name"]) || !isset($_POST["curr_sign"]) || !isset($_POST["curr_format"]))
+		fail();
+
+	$curr_format = ($_POST["curr_format"] == "on") ? 1 : 0;
+
+	if (!Currency::create($_POST["curr_name"], $_POST["curr_sign"], $curr_format))
+		fail();
+
+	setLocation("../admin/currency.php?add=ok");
+?>
