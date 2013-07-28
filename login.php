02a7adfb4706766de331ca7c6794940934e429da
Ilya Makarov <feesler@gmail.com>
2013-07-28 22:17:42 +0400

    * Update layout of login and registration pages

--- login.php
+++ login.php
@@ -34,9 +34,9 @@
 		<div class="box">
 			<h1>Log in</h1>
 			<label for="login">Username</label>
-			<div class="rdiv"><div><input id="login" name="login" type="text"></div></div>
+			<div class="stretch_input profile_input"><div><input id="login" name="login" type="text"></div></div>
 			<label for="password">Password</label>
-			<div class="rdiv"><div><input id="password" name="password" type="password"></div></div>
+			<div class="stretch_input profile_input"><div><input id="password" name="password" type="password"></div></div>
 			<div class="login_controls"><input class="btn ok_btn" type="submit" value="Log in"><span class="alter_link"><a href="./register.php">Register</a></span></div>
 		</div>
 	</div>
--- register.php
+++ register.php
@@ -34,11 +34,11 @@
 		<div class="box">
 			<h1>Registration</h1>
 			<label for="login">Account name</label>
-			<div class="rdiv"><div><input id="login" name="login" type="text"></div></div>
+			<div class="stretch_input profile_input"><div><input id="login" name="login" type="text"></div></div>
 			<label for="login">Name</label>
-			<div class="rdiv"><div><input id="name" name="name" type="text"></div></div>
+			<div class="stretch_input profile_input"><div><input id="name" name="name" type="text"></div></div>
 			<label for="password">Password</label>
-			<div class="rdiv"><div><input id="password" name="password" type="password"></div></div>
+			<div class="stretch_input profile_input"><div><input id="password" name="password" type="password"></div></div>
 			<div class="login_controls"><input class="btn ok_btn" type="submit" value="ok"><span class="alter_link"><a href="./login.php">Log in</a></span></div>
 		</div>
 	</div>
