35c0e3e9a84ce7ea26007dca0761f5ccb07d753f
Ilya Makarov <feesler@gmail.com>
2013-10-05 20:28:54 +0400

    + [PHP|JS] Add persons page

---
+++ js/persons.js
@@ -0,0 +1,148 @@
+var dwPopup;		// delete warning popup
+
+
+var persons =
+{
+	selectedArr : [],
+
+	// Return position of person in selectedArr
+	getPos : function(p_id)
+	{
+		return this.selectedArr.indexOf(p_id);
+	},
+
+
+	isSelected : function(p_id)
+	{
+		return this.selectedArr.some(function(person_id){ return person_id == p_id; });
+	},
+
+
+	selectPerson : function(p_id)
+	{
+		if (!p_id || this.isSelected(p_id))
+			return false;
+
+		this.selectedArr.push(p_id);
+		return true;
+	},
+
+
+	deselectPerson : function(p_id)
+	{
+		var p_pos = this.getPos(p_id);
+
+		if (p_pos == -1)
+			return false;
+
+		this.selectedArr.splice(p_pos, 1);
+		return true;
+	},
+
+
+	selectedCount : function()
+	{
+		return this.selectedArr.length;
+	}
+};
+
+
+// Tile click event handler
+function onTileClick(p_id)
+{
+	var tile, edit_btn, del_btn, delpersons;
+	var actDiv;
+
+	tile = ge('p_' + p_id);
+	edit_btn = ge('edit_btn');
+	del_btn = ge('del_btn');
+	delpersons = ge('delpersons');
+	if (!tile || !edit_btn || !delpersons)
+		return;
+
+	if (persons.isSelected(p_id))
+	{
+		persons.deselectPerson(p_id);
+
+		actDiv = ge('act_' + p_id);
+		if (actDiv)
+			tile.removeChild(actDiv);
+	}
+	else
+	{
+		persons.selectPerson(p_id);
+
+		actDiv = ce('div', { id : 'act_' + p_id, className : 'act', onclick : bind(onTileClick, null, p_id) });
+
+		tile.appendChild(actDiv);
+	}
+
+	show(edit_btn, (persons.selectedCount() == 1));
+	show(del_btn, (persons.selectedCount() > 0));
+
+	delpersons.value = persons.selectedArr.join();
+
+	if (persons.selectedCount() == 1)
+	{
+		if (edit_btn.firstElementChild && edit_btn.firstElementChild.tagName.toLowerCase() == 'a')
+			edit_btn.firstElementChild.href = './editperson.php?id=' + persons.selectedArr[0];
+	}
+}
+
+
+var singlePersonDeleteTitle = 'Delete person';
+var multiPersonsDeleteTitle = 'Delete persons';
+var multiPersonsDeleteMsg = 'Are you sure want to delete selected persons?<br>Debt operations will be converted into expense or income.';
+var singlePersonDeleteMsg = 'Are you sure want to delete selected person?<br>Debt operations will be converted into expense or income.';
+
+
+// Delete popup callback
+function onDeletePopup(res)
+{
+	var delform;
+
+	if (!dwPopup)
+		return;
+
+	dwPopup.close();
+	dwPopup = null;
+
+	if (res)
+	{
+		delform = ge('delform');
+		if (delform)
+			delform.submit();
+	}
+}
+
+
+// Create and show person delete warning popup
+function showDeletePopup()
+{
+	var multi;
+
+	if (persons.selectedCount() == 0)
+		return;
+
+	// check popup already created
+	if (dwPopup)
+		return;
+
+	dwPopup = new Popup();
+	if (!dwPopup)
+		return;
+
+	multi = (persons.selectedCount() > 1);
+	if (!dwPopup.create({ id : 'delete_warning',
+						title : (multi) ? multiPersonsDeleteTitle : singlePersonDeleteTitle,
+						msg : (multi) ? multiPersonsDeleteMsg : singlePersonDeleteMsg,
+						btn : { okBtn : { onclick : bind(onDeletePopup, null, true) },
+						cancelBtn : { onclick : bind(onDeletePopup, null, false) } }
+						}))
+	{
+		dwPopup = null;
+		return;
+	}
+
+	dwPopup.show();
+}
---
+++ persons.php
@@ -0,0 +1,54 @@
+<?php
+	require_once("./setup.php");
+	require_once("./class/user.php");
+	require_once("./class/person.php");
+	require_once("./class/currency.php");
+	require_once("./class/account.php");
+
+	$user_id = User::check();
+	if (!$user_id)
+		setLocation("./login.php");
+
+	$pers = new Person($user_id);
+
+	$titleString = "Jezve Money | Persons";
+
+	html("<!DOCTYPE html>");
+	html("<html>");
+	html("<head>");
+
+	html(getCommonHeaders());
+
+	html("<title>".$titleString."</title>");
+	html(getCSS("common.css"));
+	html(getCSS("tiles.css"));
+	html(getCSS("iconlink.css"));
+	html(getCSS("popup.css"));
+	html(getJS("common.js"));
+	html(getJS("popup.js"));
+	html(getJS("persons.js"));
+
+	html("</head>");
+	html("<body>");
+
+	require_once("./templates/header.php");
+
+	html_op("<div class=\"content\">");
+		html_op("<div>");
+			html_op("<div class=\"heading\">");
+				html("<h1>Persons</h1>");
+				html(getIconLink(ICON_LINK, "add_btn", "add", "New", TRUE, "./newperson.php"));
+			html_cl("</div>");
+			html("<div class=\"tiles\">".$pers->getTiles(TRUE)."</div>");
+			html_op("<div class=\"control_icons\">");
+				html(getIconLink(ICON_LINK, "edit_btn", "edit", "Edit", FALSE, "#"));
+				html(getIconLink(ICON_BUTTON, "del_btn", "del", "Delete", FALSE, "showDeletePopup();"));
+			html_cl("</div>");
+		html_cl("</div>");
+	html_cl("</div>");
+	html("<form id=\"delform\" method=\"post\" action=\"./modules/delperson.php\">");
+	html("<input id=\"delpersons\" name=\"persons\" type=\"hidden\" value=\"\">");
+	html("</form>");
+	html("</body>");
+	html("</html>");
+?>
