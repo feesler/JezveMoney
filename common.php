c608dc4053a4cbd93118d824a5931d0ebaa7e1d6
Ilya Makarov <feesler@gmail.com>
2014-02-06 02:02:15 +0400

    + [HTML] Add favicon

--- common.php
+++ common.php
@@ -47,6 +47,7 @@
 
 		$resStr .= "<meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\">\r\n";
 		$resStr .= "<meta name=\"viewport\" content=\"width=device-width,maximum-scale=1,initial-scale=1,user-scalable=0\">";
+		$resStr .= "<link rel=\"shortcut icon\" href=\"./favicon.ico\" type=\"image/x-icon\">";
 
 		return $resStr;
 	}
