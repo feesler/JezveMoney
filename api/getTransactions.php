d1f8a44afab34a4e058794e2fe570479ec546b54
Ilya Makarov <feesler@gmail.com>
2014-03-14 04:09:43 +0400

    + [PHP] Add getTransactions API

---
+++ api/getTransactions.php
@@ -0,0 +1,66 @@
+<?php
+	require_once("../setup.php");
+
+	class apiResponse
+	{
+		public $result;
+
+
+		public function render()
+		{
+			return f_json_encode($this);
+		}
+	}
+
+
+	function fail()
+	{
+		global $respObj;
+
+		$respObj->result = "fail";
+
+		echo($respObj->render());
+		exit();
+	}
+
+
+	function ok()
+	{
+		global $respObj;
+
+		$respObj->result = "ok";
+
+		echo($respObj->render());
+		exit();
+	}
+
+	$respObj = new apiResponse();
+
+	$user_id = User::check();
+	if ($user_id == 0)
+		fail();
+
+	$trans = new Transaction($user_id);
+	$acc = new Account($user_id);
+
+	$type_str = (isset($_GET["type"])) ? $_GET["type"] : "all";
+
+	$trans_type = Transaction::getStringType($type_str);
+	if (is_null($trans_type))
+		fail();
+
+	$page_num = (isset($_GET["page"]) && is_numeric($_GET["page"])) ? (intval($_GET["page"]) - 1) : 0;
+
+	$acc_id = (isset($_GET["acc_id"])) ? intval($_GET["acc_id"]) : 0;
+	if ($acc_id && !$acc->is_exist($acc_id))
+		$acc_id = 0;
+
+	$searchReq = (isset($_GET["search"]) ? $_GET["search"] : NULL);
+
+	$stDate = (isset($_GET["stdate"]) ? $_GET["stdate"] : NULL);
+	$endDate = (isset($_GET["enddate"]) ? $_GET["enddate"] : NULL);
+
+	$respObj->data = $trans->getArray($trans_type, $acc_id, TRUE, 10, $page_num, $searchReq, $stDate, $endDate, TRUE);
+
+	ok();
+?>
