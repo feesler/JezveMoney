b7c5990c8d31546e0c9633030085a183ae816d0c
Ilya Makarov <feesler@gmail.com>
2014-05-27 00:31:48 +0400

    * [PHP] Fix root directory path of design branch

--- setup.php
+++ setup.php
@@ -4,7 +4,7 @@
 	$docroot = $_SERVER["DOCUMENT_ROOT"];
 	$ruri = $_SERVER["REQUEST_URI"];
 	$userAgent = $_SERVER["HTTP_USER_AGENT"];
-	$rootdir = "/money/";
+	$rootdir = "/money_design/";
 
 	require_once($docroot.$rootdir."class/mysql.php");
 
