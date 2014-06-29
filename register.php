e54c1c9d11e1c773b22aa98ace719bb74370e9a8
Ilya Makarov <feesler@gmail.com>
2014-06-29 23:14:22 +0400

    + [HTML|PHP] Add template for register page

--- register.php
+++ register.php
@@ -9,48 +9,8 @@
 
 	$titleString = "Jezve Money | Registration";
 
-	html("<!DOCTYPE html>");
-	html("<html>");
-	html("<head>");
+	$cssArr = array("common.css", "user.css", "iconlink.css");
+	$jsArr = array("common.js", "ready.js", "main.js");
 
-	html(getCommonHeaders());
-
-	html("<title>".$titleString."</title>");
-	html(getCSS("common.css"));
-	html(getCSS("user.css"));
-	html(getCSS("iconlink.css"));
-	html(getJS("common.js"));
-	html(getJS("ready.js"));
-	html(getJS("main.js"));
-
-	if (isMessageSet())
-	{
-		html_op("<script>");
-			html("onReady(initMessage);");
-		html_op("</script>");
-	}
-
-	html("</head>");
-	html("<body>");
-
-	require_once("./templates/header.php");
-
-	html("<form action=\"./modules/register.php\" method=\"post\" onsubmit=\"return onLoginSubmit(this);\">");
-	html_op("<div class=\"layer register_layer\">");
-		html_op("<div class=\"cont\">");
-			html_op("<div class=\"box\">");
-				html("<h1>Registration</h1>");
-				html("<label for=\"login\">Account name</label>");
-				html("<div class=\"stretch_input std_input\"><div><input id=\"login\" name=\"login\" type=\"text\"></div></div>");
-				html("<label for=\"login\">Name</label>");
-				html("<div class=\"stretch_input std_input\"><div><input id=\"name\" name=\"name\" type=\"text\"></div></div>");
-				html("<label for=\"password\">Password</label>");
-				html("<div class=\"stretch_input std_input\"><div><input id=\"password\" name=\"password\" type=\"password\"></div></div>");
-				html("<div class=\"login_controls\"><input class=\"btn ok_btn\" type=\"submit\" value=\"ok\"><span class=\"alter_link\"><a href=\"./login.php\">Log in</a></span></div>");
-			html_cl("</div>");
-		html_cl("</div>");
-	html_cl("</div>");
-	html("</form>");
-	html("</body>");
-	html("</html>");
+	include("./templates/register.tpl");
 ?>
