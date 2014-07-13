71a3af2418f36e532cf8bc4b751aa75af6691435
Ilya Makarov <feesler@gmail.com>
2014-07-13 22:41:38 +0400

    + [HTML|PHP] Add template for edit account page

--- editaccount.php
+++ editaccount.php
@@ -27,121 +27,21 @@
 
 	$acc_name = $acc->getName($acc_id);
 	$acc_curr = $acc->getCurrency($acc_id);
-	$acc_bal = $acc->getInitBalance($acc_id);
-	$acc_icon = $acc->getIcon($acc_id);
+	$acc_bal = $acc->getBalance($acc_id);
+	$acc_initbal = $acc->getInitBalance($acc_id);
+	$acc_icon_id = $acc->getIcon($acc_id);
+	$acc_icon = $acc->getIconClass($acc_icon_id);
+	$balance_fmt = Currency::format($acc_bal, $acc_curr);
 
 	$curr_sign = Currency::getSign($acc_curr);
 
+	$currArr = Currency::getArray(TRUE);
+	$icons = $acc->getIconsArray();
+
 	$titleString = "Jezve Money | Edit account";
 
-	html("<!DOCTYPE html>");
-	html("<html>");
-	html("<head>");
+	$cssArr = array("common.css", "tiles.css", "iconlink.css", "ddlist.css", "popup.css");
+	$jsArr = array("common.js", "ready.js", "popup.js", "currency.js", "account.js", "ddlist.js", "main.js");
 
-	html(getCommonHeaders());
-
-	html("<title>".$titleString."</title>");
-	html(getCSS("common.css"));
-	html(getCSS("tiles.css"));
-	html(getCSS("iconlink.css"));
-	html(getCSS("ddlist.css"));
-	html(getCSS("popup.css"));
-	html(getJS("common.js"));
-	html(getJS("ready.js"));
-	html(getJS("popup.js"));
-	html(getJS("currency.js"));
-	html(getJS("account.js"));
-	html(getJS("ddlist.js"));
-	html(getJS("main.js"));
-	html("<script>");
-	echo(Currency::getArray(TRUE));
-	html("var account_id = ".$acc_id.";");
-	html("var acc_name = ".json_encode($acc_name).";");
-	html("var acc_currency = ".$acc_curr.";");
-	html("var acc_balance = ".$acc_bal.";");
-	if (isMessageSet())
-		html("onReady(initMessage);");
-	html("onReady(initControls);");
-	html("</script>");
-
-	html("</head>");
-	html("<body>");
-
-	html("<form method=\"post\" action=\"./modules/editaccount.php\" onsubmit=\"return onNewAccountSubmit(this);\">");
-	html("<input id=\"accid\" name=\"accid\" type=\"hidden\" value=\"".$acc_id."\">");
-
-	html_op("<div class=\"page\">");
-		html_op("<div class=\"page_wrapper\">");
-
-	require_once("./templates/header.php");
-
-		html_op("<div class=\"container centered\">");
-	html_op("<div class=\"content\">");
-		html_op("<div class=\"content_wrap\">");
-			html_op("<div class=\"heading h2_heading\">");
-				html("<h2>Edit account</h2>");
-				html(getIconLink(ICON_BUTTON, "del_btn", "del", "Delete", TRUE, "onDelete();"));
-			html_cl("</div>");
-
-			html_op("<div>");
-				html_op("<div class=\"non_float std_margin\">");
-					html($acc->getTile(BUTTON_TILE, $acc_id, "acc_tile"));
-				html_cl("</div>");
-
-				html_op("<div class=\"non_float std_margin\">");
-					html("<label for=\"icon\">Icon</label>");
-					html_op("<div class=\"std_input\">");
-						html_op("<div>");
-							html_op("<select id=\"icon\" name=\"icon\" onchange=\"onChangeIcon(this);\">");
-								echo($acc->getIconsList($acc_icon));
-							html_cl("</select>");
-						html_cl("</div>");
-					html_cl("</div>");
-				html_cl("</div>");
-
-				html_op("<div class=\"non_float std_margin\">");
-					html("<label for=\"accname\">Account name</label>");
-					html("<div class=\"stretch_input std_input\"><div><input id=\"accname\" name=\"accname\" type=\"text\" value=\"".$acc_name."\" oninput=\"return onAccNameInput(this);\"></div></div>");
-				html_cl("</div>");
-
-				html_op("<div class=\"non_float std_margin\">");
-					html("<label for=\"currency\">Currency</label>");
-					html_op("<div class=\"std_input\">");
-						html_op("<div>");
-							html_op("<select id=\"currency\" name=\"currency\" onchange=\"onChangeAccountCurrency(this);\">");
-								echo(Currency::getList($acc_curr));
-							html_cl("</select>");
-						html_cl("</div>");
-					html_cl("</div>");
-				html_cl("</div>");
-
-				html_op("<div class=\"non_float std_margin\">");
-					html("<label for=\"balance\">Initial balance</label>");
-					html_op("<div>");
-						html("<div class=\"curr_container\"><div class=\"btn rcurr_btn inact_rbtn\"><div id=\"currsign\">".$curr_sign."</div></div></div>");
-						html_op("<div class=\"stretch_input std_input\">");
-							html_op("<div>");
-								html("<input class=\"summ_text\" id=\"balance\" name=\"balance\" type=\"text\" value=\"".$acc_bal."\" oninput=\"return onAccBalanceInput(this);\">");
-							html_cl("</div>");
-						html_cl("</div>");
-					html_cl("</div>");
-				html_cl("</div>");
-
-				html("<div class=\"acc_controls\"><input class=\"btn ok_btn\" type=\"submit\" value=\"ok\"><a class=\"btn cancel_btn\" href=\"./accounts.php\">cancel</a></div>");
-			html_cl("</div>");
-		html_cl("</div>");
-	html_cl("</div>");
-
-			html_cl("</div>");
-		html_cl("</div>");
-	html_cl("</div>");
-
-	html("</form>");
-
-	html("<form id=\"delform\" method=\"post\" action=\"./modules/delaccount.php\">");
-	html("<input name=\"accounts\" type=\"hidden\" value=\"".$acc_id."\">");
-	html("</form>");
-
-	html("</body>");
-	html("</html>");
+	include("./templates/editaccount.tpl");
 ?>
---
+++ templates/editaccount.tpl
@@ -0,0 +1,97 @@
+<?php	include("./templates/commonhdr.tpl");	?>
+<script>
+	var currency = <?=f_json_encode($currArr)?>;
+	var account_id = <?=$acc_id?>;
+	var acc_name = <?=f_json_encode($acc_name)?>;
+	var acc_currency = <?=$acc_curr?>;
+	var acc_balance = <?=$acc_initbal?>;
+<?php	if (isMessageSet()) {	?>
+	onReady(initMessage);
+<?php	}	?>
+	onReady(initControls);
+</script>
+</head>
+<body>
+<form method="post" action="./modules/editaccount.php" onsubmit="return onNewAccountSubmit(this);">
+<input id="accid" name="accid" type="hidden" value="<?=$acc_id?>">
+<div class="page">
+	<div class="page_wrapper">
+<?php	require_once("./templates/header.php");		?>
+		<div class="container centered">
+			<div class="content">
+				<div class="content_wrap">
+					<div class="heading h2_heading">
+						<h2>Edit account</h2>
+						<div id="del_btn" class="iconlink"><button onclick="onDelete();" type="button"><span class="icon del"></span><span class="icontitle"><span>Delete</span></span></button></div>
+					</div>
+
+					<div>
+						<div class="non_float std_margin">
+							<div id="acc_tile" class="tile<?=$acc_icon?>"><button class="tilelink" type="button" onclick="onTileClick(<?=$acc_id?>);"><span><span class="acc_bal"><?=$balance_fmt?></span><span class="acc_name"><?=$acc_name?></span></span></button></div>
+						</div>
+
+						<div class="non_float std_margin">
+							<label for="icon">Icon</label>
+							<div class="std_input">
+								<div>
+									<select id="icon" name="icon" onchange="onChangeIcon(this);">
+<?php	foreach($icons as $icon_id => $icon_name) {
+			if ($icon_id == $acc_icon_id) {		?>
+										<option value="<?=$icon_id?>" selected><?=$icon_name?></option>
+<?php		} else {	?>
+										<option value="<?=$icon_id?>"><?=$icon_name?></option>
+<?php		}	
+		}		?>
+									</select>
+								</div>
+							</div>
+						</div>
+
+						<div class="non_float std_margin">
+							<label for="accname">Account name</label>
+							<div class="stretch_input std_input"><div><input id="accname" name="accname" type="text" value="<?=$acc_name?>" oninput="return onAccNameInput(this);"></div></div>
+						</div>
+
+						<div class="non_float std_margin">
+							<label for="currency">Currency</label>
+							<div class="std_input">
+								<div>
+									<select id="currency" name="currency" onchange="onChangeAccountCurrency(this);">
+<?php	foreach($currArr as $currInfo) {
+			if ($currInfo[0] == $acc_curr) {	?>
+										<option value="<?=$currInfo[0]?>" selected><?=$currInfo[1]?></option>
+<?php		} else {	?>
+										<option value="<?=$currInfo[0]?>"><?=$currInfo[1]?></option>
+<?php		}
+		}		?>
+									</select>
+								</div>
+							</div>
+						</div>
+
+						<div class="non_float std_margin">
+							<label for="balance">Initial balance</label>
+							<div>
+								<div class="curr_container"><div class="btn rcurr_btn inact_rbtn"><div id="currsign"><?=$curr_sign?></div></div></div>
+								<div class="stretch_input std_input">
+									<div>
+										<input class="summ_text" id="balance" name="balance" type="text" value="<?=$acc_initbal?>" oninput="return onAccBalanceInput(this);">
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div class="acc_controls"><input class="btn ok_btn" type="submit" value="ok"><a class="btn cancel_btn" href="./accounts.php">cancel</a></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</form>
+
+<form id="delform" method="post" action="./modules/delaccount.php">
+<input name="accounts" type="hidden" value="<?=$acc_id?>">
+</form>
+</body>
+</html>
