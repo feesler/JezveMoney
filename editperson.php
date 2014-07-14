da464c44481c0107294739a5a48f1296d62951b2
Ilya Makarov <feesler@gmail.com>
2014-07-14 13:05:48 +0400

    + [HTML|PHP] Add template for edit person page

--- editperson.php
+++ editperson.php
@@ -21,73 +21,10 @@
 
 	$pName = $person->getName($p_id);
 
-
 	$titleString = "Jezve Money | Edit person";
 
-	html("<!DOCTYPE html>");
-	html("<html>");
-	html("<head>");
-	html(getCommonHeaders());
+	$cssArr = array("common.css", "iconlink.css", "tiles.css", "popup.css");
+	$jsArr = array("common.js", "ready.js", "popup.js", "persons.js");
 
-	html("<title>".$titleString."</title>");
-	html(getCSS("common.css"));
-	html(getCSS("iconlink.css"));
-	html(getCSS("tiles.css"));
-	html(getCSS("iconlink.css"));
-	html(getCSS("popup.css"));
-	html(getJS("common.js"));
-	html(getJS("ready.js"));
-	html(getJS("popup.js"));
-	html(getJS("persons.js"));
-
-	html("<script>");
-	html("var person_id = ".$p_id.";");
-	html("var personName = ".json_encode($pName).";");
-	if (isMessageSet())
-		html("onReady(initMessage);");
-	html("</script>");
-
-	html("</head>");
-	html("<body>");
-
-	html("<form method=\"post\" action=\"./modules/editperson.php\" onsubmit=\"return onEditPersonSubmit(this);\">");
-	html("<input id=\"pid\" name=\"pid\" type=\"hidden\" value=\"".$p_id."\">");
-
-	html_op("<div class=\"page\">");
-		html_op("<div class=\"page_wrapper\">");
-
-	require_once("./templates/header.tpl");
-
-		html_op("<div class=\"container centered\">");
-	html_op("<div class=\"content\">");
-		html_op("<div class=\"content_wrap\">");
-			html_op("<div class=\"heading h2_heading\">");
-				html("<h2>Edit person</h2>");
-				html(getIconLink(ICON_BUTTON, "del_btn", "del", "Delete", TRUE, "onDelete();"));
-			html_cl("</div>");
-			html_op("<div>");
-				html_op("<div class=\"non_float std_margin\">");
-					html("<label for=\"pname\">Person name</label>");
-					html_op("<div class=\"stretch_input std_input\">");
-						html("<div><input id=\"pname\" name=\"pname\" type=\"text\" value =\"".$pName."\"></div>");
-					html_cl("</div>");
-				html_cl("</div>");
-
-				html("<div class=\"acc_controls\"><input class=\"btn ok_btn\" type=\"submit\" value=\"ok\"><a class=\"btn cancel_btn\" href=\"./person.php\">cancel</a></div>");
-			html_cl("</div>");
-		html_cl("</div>");
-	html_cl("</div>");
-
-			html_cl("</div>");
-		html_cl("</div>");
-	html_cl("</div>");
-
-	html("</form>");
-
-	html("<form id=\"delform\" method=\"post\" action=\"./modules/delperson.php\">");
-	html("<input name=\"persons\" type=\"hidden\" value=\"".$p_id."\">");
-	html("</form>");
-
-	html("</body>");
-	html("</html>");
+	include("./templates/editperson.tpl");
 ?>
