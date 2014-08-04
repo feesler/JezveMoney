07566c0a956dbf4eab3d2f3ff4d33d0822fc7dd9
Ilya Makarov <feesler@gmail.com>
2014-08-04 23:55:34 +0400

    * [JS] Fix getValues() function for debt with no account case

--- js/transaction.js
+++ js/transaction.js
@@ -811,12 +811,12 @@ function getValues()
 		}
 		else			// person take from us; person account is destination
 		{
-			S1 = getBalanceOfAccount(accid.value);
-			S2 = resbal_d.value;
+			S1_d = getCurPersonBalance(trans_curr);
+			S2_d = resbal.value;
 			if (!noAccount)
 			{
-				S1_d = getCurPersonBalance(trans_curr);
-				S2_d = resbal.value;
+				S1 = getBalanceOfAccount(accid.value);
+				S2 = resbal_d.value;
 			}
 		}
 	}
