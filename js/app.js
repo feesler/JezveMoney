bae1f825d9fc3e21fd3d9feefa3a4a06586798e8
Ilya Makarov <feesler@gmail.com>
2014-11-30 19:45:02 +0300

    * [JS] Move JezveMoney specific functions from common.js to app.js

---
+++ js/app.js
@@ -0,0 +1,91 @@
+// Hide usem menu popup
+function hidePopup()
+{
+	show('menupopup', false);
+	setEmptyClick();
+}
+
+
+// Show/hide user menu by click
+function onUserClick()
+{
+	if (isVisible('menupopup'))
+	{
+		hidePopup();
+	}
+	else
+	{
+		show('menupopup', true);
+		setEmptyClick(hidePopup, ['menupopup', 'userbtn']);
+	}
+}
+
+
+// Close message box
+function onCloseMessage()
+{
+	re('action_msg');
+	setEmptyClick();
+}
+
+
+// Initialization of message hiding
+function initMessage()
+{
+	setEmptyClick(onCloseMessage, ['action_msg']);
+}
+
+
+// Fix string to correct float number format
+function fixFloat(str)
+{
+	if (typeof(str) == "string")
+		return str.replace(/,/g, '.');
+	else if (typeof(str) == "number")
+		return str;
+	else
+		return null;
+}
+
+
+// Check specified key code is float dot ('.' or ',')
+function isDot(code)
+{
+	return (code == 44 || code == 46);
+}
+
+
+// Check specified string have float dot ('.' or ',')
+function haveDot(str)
+{
+	return (typeof(str) == "string" && (str.indexOf('.') != -1 || str.indexOf(',') != -1));
+}
+
+
+function onFieldKey(event, obj)
+{
+	var code, isDig, isDel;
+
+	if (!obj)
+		return false;
+
+	code = getCode(event);
+	isDig = isDigit(code);
+	isDel = (isDig && event.which == 0);
+
+	if (!isDig)
+		return false;
+
+	if (!isDel)
+	{
+	// exclude ',' or '.' at start
+		if ((obj.value == '' || (obj.value != '' && getCaretPos(obj) == 0)) && isDot(code))
+			return false;
+
+	// exclude ',' or '.' in one string
+		if (obj.value != '' && isDot(code) && haveDot(obj.value))
+			return false;
+	}
+
+	return true;
+}
--- js/common.js
+++ js/common.js
@@ -157,18 +157,6 @@ function dw(a)
 }
 
 
-// Fix string to correct float number format
-function fixFloat(str)
-{
-	if (typeof(str) == "string")
-		return str.replace(/,/g, '.');
-	else if (typeof(str) == "number")
-		return str;
-	else
-		return null;
-}
-
-
 // Check is specified string is number
 function isNum(val)
 {
@@ -282,49 +270,6 @@ function getCaretPos(obj)
 	}
 
 	return 0;
-}
-
-
-// Check specified key code is float dot ('.' or ',')
-function isDot(code)
-{
-	return (code == 44 || code == 46);
-}
-
-
-// Check specified string have float dot ('.' or ',')
-function haveDot(str)
-{
-	return (typeof(str) == "string" && (str.indexOf('.') != -1 || str.indexOf(',') != -1));
-}
-
-
-function onFieldKey(event, obj)
-{
-	var code, isDig, isDel;
-
-	if (!obj)
-		return false;
-
-	code = getCode(event);
-	isDig = isDigit(code);
-	isDel = (isDig && event.which == 0);
-
-	if (!isDig)
-		return false;
-
-	if (!isDel)
-	{
-	// exclude ',' or '.' at start
-		if ((obj.value == '' || (obj.value != '' && getCaretPos(obj) == 0)) && isDot(code))
-			return false;
-
-	// exclude ',' or '.' in one string
-		if (obj.value != '' && isDot(code) && haveDot(obj.value))
-			return false;
-	}
-
-	return true;
 }
 
 
@@ -645,29 +590,6 @@ function setEmptyClick(callback, elem)
 }
 
 
-// Hide usem menu popup
-function hidePopup()
-{
-	show('menupopup', false);
-	setEmptyClick();
-}
-
-
-// Show/hide user menu by click
-function onUserClick()
-{
-	if (isVisible('menupopup'))
-	{
-		hidePopup();
-	}
-	else
-	{
-		show('menupopup', true);
-		setEmptyClick(hidePopup, ['menupopup', 'userbtn']);
-	}
-}
-
-
 // Calculate offset of element
 function getOffset(elem)
 {
@@ -796,21 +718,6 @@ function getPageScroll()
 
 		return { top: top, left: left };
 	}
-}
-
-
-// Close message box
-function onCloseMessage()
-{
-	re('action_msg');
-	setEmptyClick();
-}
-
-
-// Initialization of message hiding
-function initMessage()
-{
-	setEmptyClick(onCloseMessage, ['action_msg']);
 }
 
 
