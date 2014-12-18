5a5f60a407b0969703ce3d69257af15bb3d40883
Ilya Makarov <feesler@gmail.com>
2014-12-18 03:27:53 +0300

    * [PHP] Fix ::fail() method call from AccountsController::createAccount()

--- Controller/accounts.php
+++ Controller/accounts.php
@@ -104,12 +104,12 @@ class AccountsController extends Controller
 		$defMsg = ERR_ACCOUNT_CREATE;
 
 		if (!isset($_POST["accname"]) || !isset($_POST["balance"]) || !isset($_POST["currency"]) || !isset($_POST["icon"]))
-			fail($defMsg);
+			$this->fail($defMsg);
 
 		$acc = new Account($user_id);
 		$owner_id = $u->getOwner($user_id);
 		if (!$acc->create($owner_id, $_POST["accname"], $_POST["balance"], $_POST["currency"], $_POST["icon"]))
-			fail($defMsg);
+			$this->fail($defMsg);
 
 		setMessage(MSG_ACCOUNT_CREATE);
 
