e3f02b7b755ddb7c3d5e72cebbe0f303dad4b949
Ilya Makarov <feesler@gmail.com>
2014-12-20 23:55:13 +0300

    * [PHP] Clean up

--- index.php
+++ index.php
@@ -1,15 +1,6 @@
 <?php
 	require_once("./system/setup.php");
 
-wlog("route: ".$route);
-
-	$route = trim($route, "/\\");
-	$routeParts = explode("/", $route);
-
-foreach($routeParts as $ind => $rpart)
-	wlog($ind." => ".$rpart);
-
-
 	$controller = NULL;
 	$action = NULL;
 
@@ -27,12 +18,14 @@ foreach($routeParts as $ind => $rpart)
 	$actionsMap = array("new" => "create",
 						"edit" => "update");
 
+	// Parse route
+	$route = trim($route, "/\\");
+	$routeParts = explode("/", $route);
+
 	// Prepare controller
 	$contrStr = array_shift($routeParts);
 	if (!$contrStr)
 		$contrStr = "index";
-
-wlog("contrStr: ".$contrStr);
 
 	if (!isset($controllersMap[$contrStr]))
 		setLocation(BASEURL);
@@ -67,105 +60,3 @@ wlog("contrStr: ".$contrStr);
 
 	if (method_exists($controller, $action))
 		$controller->$action();
-
-
-/*
-	if ($contrStr == "accounts")
-	{
-		$controller = new AccountsController();
-
-		if ($action == "new")
-			$controller->create();
-		else if ($action == "edit")
-		{
-			$_GET["id"] = $routeParts[2];
-			$controller->update();
-		}
-		else if ($action == "del")
-		{
-			$controller->del();
-		}
-		else if ($action == "reset")
-		{
-			$controller->reset();
-		}
-		else if (!is_null($action))
-			setLocation(BASEURL."accounts/");
-	}
-	else if ($contrStr == "persons")
-	{
-		$controller = new PersonsController();
-
-		if ($action == "new")
-			$controller->create();
-		else if ($action == "edit")
-		{
-			$_GET["id"] = $routeParts[2];
-			$controller->update();
-		}
-		else if ($action == "del")
-		{
-			$controller->del();
-		}
-		else if (!is_null($action))
-			setLocation(BASEURL."persons/");
-	}
-	else if ($contrStr == "transactions")
-	{
-		$controller = new TransactionsController();
-
-		if ($action == "new")
-			$controller->create();
-		else if ($action == "edit")
-		{
-			$_GET["id"] = $routeParts[2];
-			$controller->update();
-		}
-		else if ($action == "del")
-		{
-			$controller->del();
-		}
-		else if (!is_null($action))
-			setLocation(BASEURL."transactions/");
-	}
-	else if ($contrStr == "profile")
-	{
-		$controller = new ProfileController();
-
-		$controller->action = $action;
-
-		if ($action == "changename")
-			$controller->changeName();
-		else if ($action == "changepass")
-		{
-			$controller->changePass();
-		}
-		else if ($action == "resetall")
-		{
-			$controller->resetAll();
-		}
-		else if (!is_null($action))
-			setLocation(BASEURL.$contrStr."/");
-	}
-	else if ($contrStr == "statistics")
-	{
-		$controller = new StatisticsController();
-
-		if (!is_null($action))
-			setLocation(BASEURL.$contrStr."/");
-	}
-	else if ($contrStr == "login" || $contrStr == "logout" || $contrStr == "register")
-	{
-		$controller = new UserController();
-
-		$controller->$contrStr();
-	}
-
-	if (is_null($controller))
-		$controller = new MainController();
-
-wlog("action: ".(is_null($action) ? "NULL" : $action));
-
-	if (is_null($action))
-		$controller->index();
-*/
