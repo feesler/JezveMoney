c87d9c16dcdbd3eddcf7b69129e7bb8eb5e25770
Ilya Makarov <feesler@gmail.com>
2015-01-03 03:43:16 +0300

    * [JS] Move Person related functions to person.js

---
+++ view/js/person.js
@@ -0,0 +1,74 @@
+// Return object for specified person
+function getPersonObject(person_id)
+{
+	var pObj = null, p_id;
+
+	p_id = parseInt(person_id);
+	if (!persons || !p_id)
+		return null;
+
+	persons.some(function(person)
+	{
+		if (person[0] == p_id)
+			pObj = person;
+		return (person[0] == p_id);
+	});
+
+	return pObj;
+}
+
+
+// Return name of person
+function getPersonName(p_id)
+{
+	var person;
+
+	person = getPersonObject(p_id);
+	if (!person || !isArray(person) || person.length < 3)
+		return null;
+
+	return person[1];
+}
+
+
+// Return array of balance
+function getPersonBalance(p_id)
+{
+	var person, resArr = [];
+
+	person = getPersonObject(p_id);
+	if (!person || !isArray(person) || person.length < 3 || !isArray(person[2]))
+		return null;
+
+	person[2].forEach(function(acc)
+	{
+		resArr.push(formatCurrency(acc[2], acc[1]));
+	});
+
+	return resArr;
+}
+
+
+// Return balance of current person in specified currency
+function getCurPersonBalance(curr_id)
+{
+	var personid, p_id, person, resBal = 0.0;
+
+	personid = ge('person_id');
+	if (!personid || !curr_id)
+		return resBal;
+	person = getPersonObject(personid.value);
+	if (!person || !isArray(person) || person.length < 3 || !isArray(person[2]))
+		return resBal;
+
+	// check person have account in specified currency
+	person[2].some(function(acc)
+	{
+		if (acc[1] == curr_id)
+			resBal = acc[2];
+
+		return (acc[1] == curr_id);
+	});
+
+	return resBal;
+}
