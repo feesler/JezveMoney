87c1d94937481e921cd430350da317aff0a0890f
Ilya Makarov <feesler@gmail.com>
2019-11-29 15:24:57 +0300

    + Start to implement transactions API tests

--- src/admin/view/js/tests/run/api.js
+++ src/admin/view/js/tests/run/api.js
@@ -104,6 +104,79 @@ var runAPI = (function()
 	}
 
 
+	// Create person with specified params (name)
+	// And check expected state of app
+	async function apiCreateTransactionTest(params)
+	{
+		let transaction_id = 0;
+
+		await App.test('Create transaction', async () =>
+		{
+			let trBefore = await api.transaction.list();
+			if (!App.isArray(trBefore))
+				return false;
+
+			let expTransObj = App.copyObject(params);
+			let isDebt = (expTransObj.transtype == App.DEBT);
+			if (isDebt)
+			{
+				let personsList = await api.person.read(expTransObj.person_id);
+				let personObj = App.idSearch(personsList, expTransObj.person_id);
+				if (!personObj)
+					throw new Error('Person not found');
+
+				let reqCurr = (expTransObj.debtop == 1) ? expTransObj.src_curr : expTransObj.dest_curr;
+				let personAcc = personObj.accounts.find(item => (item.curr_id == reqCurr));
+
+				if (expTransObj.debtop == 1)
+				{
+					if (personAcc)
+						expTransObj.src_id = personAcc.id;
+					expTransObj.dest_id = expTransObj.acc_id;
+				}
+				else
+				{
+					expTransObj.src_id = expTransObj.acc_id;
+					if (personAcc)
+						expTransObj.dest_id = personAcc.id;
+				}
+
+				delete expTransObj.debtop;
+				delete expTransObj.person_id;
+				delete expTransObj.acc_id;
+			}
+
+			expTransObj.type = expTransObj.transtype;
+			delete expTransObj.transtype;
+
+			expTransObj.comment = expTransObj.comm;
+			delete expTransObj.comm;
+
+			let createRes = await api.transaction.create(params);
+			if (!createRes || !createRes.id)
+				return false;
+
+			transaction_id = createRes.id;
+
+			let trList = await api.transaction.list();
+			if (!App.isArray(trList))
+				return false;
+
+			if (trList.length != trBefore.length + 1)
+				throw new Error('Length of transactions list must increase');
+
+			if (App.idSearch(trBefore, transaction_id))
+				throw new Error('Already exist transaction returned');
+
+			let transObj = App.idSearch(trList, transaction_id);
+
+			return App.checkObjValue(transObj, expTransObj);
+		}, env);
+
+		return transaction_id;
+	}
+
+
 	async function runTests(view, App)
 	{
 		console.log('api.run()');
@@ -149,6 +222,8 @@ var runAPI = (function()
 		let PERSON_Y = await apiCreatePersonTest({ name : 'Y' });
 
 
+		let now = new Date();
+
 		env.setBlock('Transactions', 1);
 
 		await App.test('Transactions list', async () => {
@@ -156,6 +231,17 @@ var runAPI = (function()
 
 			return App.isArray(trList) && trList.length == 0;
 		}, env);
+
+		let TR_EXPENSE_1 = await apiCreateTransactionTest({ transtype : App.EXPENSE,
+															src_id : ACC_RU,
+															dest_id : 0,
+														 	src_amount : 100,
+														 	dest_amount : 100,
+															src_curr : RUB,
+															dest_curr : RUB,
+															date : App.formatDate(now),
+														 	comm : '' });
+
 	}
 
 
