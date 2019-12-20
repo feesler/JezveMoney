46d3a90b759e1dd87df47e5f90d51e2080c203ca
Ilya Makarov <feesler@gmail.com>
2019-12-20 14:16:04 +0300

    * Move remain functions into Application class

--- src/admin/view/js/tests/main.js
+++ src/admin/view/js/tests/main.js
@@ -106,13 +106,13 @@ class Application
 
 		await this.init();
 
-		await apiTests(this);
-		await profileTests(this);
-		await accountTests(this);
-		await personTests(this);
-		await transactionTests(this);
+		await this.apiTests();
+		await this.profileTests();
+		await this.accountTests();
+		await this.personTests();
+		await this.transactionTests();
 		await statistics.run(this);
-		await transactionsListTests(this);
+		await this.transactionsListTests();
 	}
 
 
@@ -127,263 +127,262 @@ class Application
 
 		this.notify();
 	}
+
+
+	async profileTests()
+	{
+		this.view.setBlock('Profile tests', 1);
+
+		await profile.register(this, { login : 'newuser', name : 'Newbie', password : '12345' });
+		await profile.deleteProfile(this);
+		await profile.relogin(this, this.config.testUser);
+		await profile.resetAll(this);
+		await profile.changeName(this);
+		await profile.changePass(this);
+	}
+
+
+	async accountTests()
+	{
+		this.view.setBlock('Accounts', 1);
+
+		await this.goToMainView();
+		await this.view.goToAccounts();
+		await this.view.goToCreateAccount();
+		await accounts.createAccount1(this);
+		await this.view.goToCreateAccount();
+		await accounts.createAccount2(this);
+		await this.view.goToUpdateAccount(0);
+		await accounts.editAccount1(this);
+		await this.view.goToCreateAccount();
+		await accounts.create(this, { name : 'acc_3', curr_id : 1, balance : '500.99', icon : 2 });
+		await accounts.del(this, [0, 1]);
+		await this.view.goToCreateAccount();
+		await accounts.create(this, { name : 'acc RUB', curr_id : 1, balance : '500.99', icon : 5 });
+		await this.view.goToCreateAccount();
+		await accounts.create(this, { name : 'acc USD', curr_id : 2, balance : '500.99', icon : 4 });
+		await this.view.goToCreateAccount();
+		await accounts.create(this, { name : 'acc EUR', curr_id : 3, balance : '10000.99', icon : 3 });
+		await this.view.goToCreateAccount();
+		await accounts.create(this, { name : 'card RUB', curr_id : 1, balance : '35000.40', icon : 3 });
+	}
+
+
+	async personTests()
+	{
+		this.view.setBlock('Persons', 1);
+
+		await this.goToMainView();
+		await this.view.goToPersons();
+		await persons.checkInitial(this);
+		await persons.create(this, 'Alex');
+		await persons.create(this, 'Maria');
+		await persons.create(this, 'Johnny');
+		await persons.create(this, 'Иван');
+		await persons.update(this, 3, 'Ivan<');
+		await persons.del(this, [0, 2]);
+	}
+
+
+	async transactionTests()
+	{
+		this.view.setBlock('Transactions', 1);
+
+		await this.createTransactionTests();
+		await this.updateTransactionTests();
+		await this.deleteTransactionTests();
+	}
+
+
+	async createTransactionTests()
+	{
+		this.view.setBlock('Create transaction', 1);
+
+		await this.goToMainView();
+		await this.view.goToNewTransactionByAccount(0);
+		await transactions.expense.stateLoop(this);
+		await this.runCreateExpenseTests();
+
+		await this.view.goToNewTransactionByAccount(0);
+		await this.view.changeTransactionType(App.INCOME);
+		await transactions.income.stateLoop(this);
+		await this.runCreateIncomeTests();
+
+		await this.view.goToNewTransactionByAccount(0);
+		await this.view.changeTransactionType(App.TRANSFER);
+		await transactions.transfer.stateLoop(this);
+		await this.runCreateTransferTests();
+
+		await this.view.goToNewTransactionByAccount(0);
+		await this.view.changeTransactionType(App.DEBT);
+		await transactions.debt.stateLoop(this);
+		await this.runCreateDebtTests();
+	}
+
+
+	async updateTransactionTests()
+	{
+		this.view.setBlock('Update transaction', 1);
+
+		await this.runUpdateExpenseTests();
+		await this.runUpdateIncomeTests();
+		await this.runUpdateTransferTests();
+		await this.runUpdateDebtTests();
+	}
+
+
+	async deleteTransactionTests()
+	{
+		this.view.setBlock('Delete transaction', 1);
+
+		await this.runDeleteExpenseTests();
+		await this.runDeleteIncomeTests();
+		await this.runDeleteTransferTests();
+		await this.runDeleteDebtTests();
+	}
+
+
+	async runCreateExpenseTests()
+	{
+		this.view.setBlock('Create expense transactions', 1);
+
+		await transactions.expense.create(this, 0, 0, { destAmount : '123.7801' })
+		await transactions.expense.create(this, 3, 2, { srcAmount : '100', destAmount : '7013.21', destCurr : 1 });
+		await transactions.expense.create(this, 1, 0, { destAmount : '0.01' });
+		await transactions.expense.create(this, 1, 0, { srcAcc : 4, destAmount : '99.99' });
+	}
+
+
+	async runCreateIncomeTests()
+	{
+		this.view.setBlock('Create income transactions', 1);
+
+		await transactions.income.create(this, 0, 0, { srcAmount : '10023.7801' });
+		await transactions.income.create(this, 3, 2, { srcAmount : '7013.21', destAmount : '100', srcCurr : 1 });
+		await transactions.income.create(this, 1, 0, { srcAmount : '0.01' });
+		await transactions.income.create(this, 1, 0, { destAcc : 4, srcAmount : '99.99' });
+	}
+
+
+	async runCreateTransferTests()
+	{
+		this.view.setBlock('Create transfer transactions', 1);
+
+		await transactions.transfer.create(this, 0, { srcAmount : '1000' })
+		await transactions.transfer.create(this, 0, { destAcc : 2, srcAmount : '11.4', destAmount : '10' });
+		await transactions.transfer.create(this, 0, { srcAcc : 1, destAcc : 3, srcAmount : '5.0301', destAmount : '4.7614' });
+		await transactions.transfer.create(this, 0, { srcAcc : 2, srcAmount : '10', destAmount : '9.75' });
+		await transactions.transfer.create(this, 0, { destAcc : 3, srcAmount : '10', destAmount : '9.50' });
+	}
+
+
+	async runCreateDebtTests()
+	{
+		this.view.setBlock('Submit debt transactions', 1);
+
+		await transactions.debt.create(this, 0, { srcAmount : '1000' });
+		await transactions.debt.create(this, 0, { debtType : false, acc : 2, srcAmount : '200' });
+		await transactions.debt.create(this, 0, { debtType : true, acc : 3, srcAmount : '100.0101' });
+		await transactions.debt.create(this, 0, { debtType : false, person : 1, acc : 3, srcAmount : '10' });
+		await transactions.debt.create(this, 0, { acc : null, srcAmount : '105' });
+		await transactions.debt.create(this, 0, { debtType : false, person : 1, acc : null, srcAmount : '105' });
+	}
+
+
+	async runUpdateExpenseTests()
+	{
+		this.view.setBlock('Update expense transactions', 2);
+
+		await transactions.expense.update(this, 3, { destAmount : '124.7701' });
+		await transactions.expense.update(this, 2, { srcAmount : '101', destAmount : '7065.30', destCurr : 1 });
+		await transactions.expense.update(this, 1, { destAmount : '0.02' });
+		await transactions.expense.update(this, 0, { srcAcc : 3, destAmount : '99.9' });
+	}
+
+
+	async runUpdateIncomeTests()
+	{
+		this.view.setBlock('Update income transactions', 2);
+
+		await transactions.income.update(this, 0, { srcAmount : '100.001' });
+		await transactions.income.update(this, 1, { srcAmount : '0.02' });
+		await transactions.income.update(this, 2, { srcAmount : '7065.30', destAmount : '101', srcCurr : 1 });
+		await transactions.income.update(this, 3, { destAcc : 3, srcAmount : '99.9' });
+	}
+
+
+	async runUpdateTransferTests()
+	{
+		this.view.setBlock('Update transfer transactions', 2);
+
+		await transactions.transfer.update(this, 0, { destAcc : 0, srcAmount : '11' });
+		await transactions.transfer.update(this, 1, { srcAcc : 2, srcAmount : '100', destAmount : '97.55' });
+		await transactions.transfer.update(this, 2, { srcAcc : 3, srcAmount : '5.0301' });
+		await transactions.transfer.update(this, 3, { srcAcc : 0, srcAmount : '50', destAmount : '0.82' });
+		await transactions.transfer.update(this, 4, { srcAmount : '1050.01' });
+	}
+
+
+	async runUpdateDebtTests()
+	{
+		this.view.setBlock('Update debt transactions', 2);
+
+		await transactions.debt.update(this, 0, { person : 0, srcAmount : '105' });
+		await transactions.debt.update(this, 1, { acc : 1, srcAmount : '105' });
+		await transactions.debt.update(this, 2, { debtType : true, srcAmount : '10' });
+		await transactions.debt.update(this, 3, { debtType : false, acc : 2, srcAmount : '200.0202' });
+		await transactions.debt.update(this, 4, { acc : null, srcAmount : '200' });
+		await transactions.debt.update(this, 5, { srcAmount : '1001' });
+	}
+
+
+	async runDeleteExpenseTests()
+	{
+		this.view.setBlock('Delete expense transactions', 2);
+
+		await transactions.del(this, App.EXPENSE, [0]);
+		await transactions.del(this, App.EXPENSE, [0, 1]);
+	}
+
+
+	async runDeleteIncomeTests()
+	{
+		this.view.setBlock('Delete income transactions', 2);
+
+		await transactions.del(this, App.INCOME, [0]);
+		await transactions.del(this, App.INCOME, [0, 1, 2]);
+	}
+
+
+	async runDeleteTransferTests()
+	{
+		this.view.setBlock('Delete transfer transactions', 2);
+
+		await transactions.del(this, App.TRANSFER, [1]);
+		await transactions.del(this, App.TRANSFER, [0, 2]);
+	}
+
+
+	async runDeleteDebtTests()
+	{
+		this.view.setBlock('Delete debt transactions', 2);
+
+		await transactions.del(this, App.DEBT, [0]);
+		await transactions.del(this, App.DEBT, [0, 1]);
+	}
+
+
+	async apiTests()
+	{
+		await runAPI.run(this);
+	}
+
+
+	async transactionsListTests()
+	{
+		await runTransList.run(this);
+	}
 }
-
-
-async function profileTests(app)
-{
-	app.view.setBlock('Profile tests', 1);
-
-	await profile.register(app, { login : 'newuser', name : 'Newbie', password : '12345' });
-	await profile.deleteProfile(app);
-	await profile.relogin(app, app.config.testUser);
-	await profile.resetAll(app);
-	await profile.changeName(app);
-	await profile.changePass(app);
-}
-
-
-async function accountTests(app)
-{
-	app.view.setBlock('Accounts', 1);
-
-	await app.goToMainView();
-	await app.view.goToAccounts();
-	await app.view.goToCreateAccount();
-	await accounts.createAccount1(app);
-	await app.view.goToCreateAccount();
-	await accounts.createAccount2(app);
-	await app.view.goToUpdateAccount(0);
-	await accounts.editAccount1(app);
-	await app.view.goToCreateAccount();
-	await accounts.create(app, { name : 'acc_3', curr_id : 1, balance : '500.99', icon : 2 });
-	await accounts.del(app, [0, 1]);
-	await app.view.goToCreateAccount();
-	await accounts.create(app, { name : 'acc RUB', curr_id : 1, balance : '500.99', icon : 5 });
-	await app.view.goToCreateAccount();
-	await accounts.create(app, { name : 'acc USD', curr_id : 2, balance : '500.99', icon : 4 });
-	await app.view.goToCreateAccount();
-	await accounts.create(app, { name : 'acc EUR', curr_id : 3, balance : '10000.99', icon : 3 });
-	await app.view.goToCreateAccount();
-	await accounts.create(app, { name : 'card RUB', curr_id : 1, balance : '35000.40', icon : 3 });
-}
-
-
-async function personTests(app)
-{
-	app.view.setBlock('Persons', 1);
-
-	await app.goToMainView();
-	await app.view.goToPersons();
-	await persons.checkInitial(app);
-	await persons.create(app, 'Alex');
-	await persons.create(app, 'Maria');
-	await persons.create(app, 'Johnny');
-	await persons.create(app, 'Иван');
-	await persons.update(app, 3, 'Ivan<');
-	await persons.del(app, [0, 2]);
-}
-
-
-async function transactionTests(app)
-{
-	app.view.setBlock('Transactions', 1);
-
-	await createTransactionTests(app);
-	await updateTransactionTests(app);
-	await deleteTransactionTests(app);
-}
-
-
-async function createTransactionTests(app)
-{
-	app.view.setBlock('Create transaction', 1);
-
-	await app.goToMainView();
-	await app.view.goToNewTransactionByAccount(0);
-	await transactions.expense.stateLoop(app);
-	await runCreateExpenseTests(app);
-
-	await app.view.goToNewTransactionByAccount(0);
-	await app.view.changeTransactionType(App.INCOME);
-	await transactions.income.stateLoop(app);
-	await runCreateIncomeTests(app);
-
-	await app.view.goToNewTransactionByAccount(0);
-	await app.view.changeTransactionType(App.TRANSFER);
-	await transactions.transfer.stateLoop(app);
-	await runCreateTransferTests(app);
-
-	await app.view.goToNewTransactionByAccount(0);
-	await app.view.changeTransactionType(App.DEBT);
-	await transactions.debt.stateLoop(app);
-	await runCreateDebtTests(app);
-}
-
-
-async function updateTransactionTests(app)
-{
-	app.view.setBlock('Update transaction', 1);
-
-	await runUpdateExpenseTests(app);
-	await runUpdateIncomeTests(app);
-	await runUpdateTransferTests(app);
-	await runUpdateDebtTests(app);
-}
-
-
-async function deleteTransactionTests(app)
-{
-	app.view.setBlock('Delete transaction', 1);
-
-	await runDeleteExpenseTests(app);
-	await runDeleteIncomeTests(app);
-	await runDeleteTransferTests(app);
-	await runDeleteDebtTests(app);
-}
-
-
-async function runCreateExpenseTests(app)
-{
-	app.view.setBlock('Create expense transactions', 1);
-
-	await transactions.expense.create(app, 0, 0, { destAmount : '123.7801' })
-	await transactions.expense.create(app, 3, 2, { srcAmount : '100', destAmount : '7013.21', destCurr : 1 });
-	await transactions.expense.create(app, 1, 0, { destAmount : '0.01' });
-	await transactions.expense.create(app, 1, 0, { srcAcc : 4, destAmount : '99.99' });
-}
-
-
-async function runCreateIncomeTests(app)
-{
-	app.view.setBlock('Create income transactions', 1);
-
-	await transactions.income.create(app, 0, 0, { srcAmount : '10023.7801' });
-	await transactions.income.create(app, 3, 2, { srcAmount : '7013.21', destAmount : '100', srcCurr : 1 });
-	await transactions.income.create(app, 1, 0, { srcAmount : '0.01' });
-	await transactions.income.create(app, 1, 0, { destAcc : 4, srcAmount : '99.99' });
-}
-
-
-async function runCreateTransferTests(app)
-{
-	app.view.setBlock('Create transfer transactions', 1);
-
-	await transactions.transfer.create(app, 0, { srcAmount : '1000' })
-	await transactions.transfer.create(app, 0, { destAcc : 2, srcAmount : '11.4', destAmount : '10' });
-	await transactions.transfer.create(app, 0, { srcAcc : 1, destAcc : 3, srcAmount : '5.0301', destAmount : '4.7614' });
-	await transactions.transfer.create(app, 0, { srcAcc : 2, srcAmount : '10', destAmount : '9.75' });
-	await transactions.transfer.create(app, 0, { destAcc : 3, srcAmount : '10', destAmount : '9.50' });
-}
-
-
-async function runCreateDebtTests(app)
-{
-	app.view.setBlock('Submit debt transactions', 1);
-
-	await transactions.debt.create(app, 0, { srcAmount : '1000' });
-	await transactions.debt.create(app, 0, { debtType : false, acc : 2, srcAmount : '200' });
-	await transactions.debt.create(app, 0, { debtType : true, acc : 3, srcAmount : '100.0101' });
-	await transactions.debt.create(app, 0, { debtType : false, person : 1, acc : 3, srcAmount : '10' });
-	await transactions.debt.create(app, 0, { acc : null, srcAmount : '105' });
-	await transactions.debt.create(app, 0, { debtType : false, person : 1, acc : null, srcAmount : '105' });
-}
-
-
-async function runUpdateExpenseTests(app)
-{
-	app.view.setBlock('Update expense transactions', 2);
-
-	await transactions.expense.update(app, 3, { destAmount : '124.7701' });
-	await transactions.expense.update(app, 2, { srcAmount : '101', destAmount : '7065.30', destCurr : 1 });
-	await transactions.expense.update(app, 1, { destAmount : '0.02' });
-	await transactions.expense.update(app, 0, { srcAcc : 3, destAmount : '99.9' });
-}
-
-
-async function runUpdateIncomeTests(app)
-{
-	app.view.setBlock('Update income transactions', 2);
-
-	await transactions.income.update(app, 0, { srcAmount : '100.001' });
-	await transactions.income.update(app, 1, { srcAmount : '0.02' });
-	await transactions.income.update(app, 2, { srcAmount : '7065.30', destAmount : '101', srcCurr : 1 });
-	await transactions.income.update(app, 3, { destAcc : 3, srcAmount : '99.9' });
-}
-
-
-async function runUpdateTransferTests(app)
-{
-	app.view.setBlock('Update transfer transactions', 2);
-
-	await transactions.transfer.update(app, 0, { destAcc : 0, srcAmount : '11' });
-	await transactions.transfer.update(app, 1, { srcAcc : 2, srcAmount : '100', destAmount : '97.55' });
-	await transactions.transfer.update(app, 2, { srcAcc : 3, srcAmount : '5.0301' });
-	await transactions.transfer.update(app, 3, { srcAcc : 0, srcAmount : '50', destAmount : '0.82' });
-	await transactions.transfer.update(app, 4, { srcAmount : '1050.01' });
-}
-
-
-async function runUpdateDebtTests(app)
-{
-	app.view.setBlock('Update debt transactions', 2);
-
-	await transactions.debt.update(app, 0, { person : 0, srcAmount : '105' });
-	await transactions.debt.update(app, 1, { acc : 1, srcAmount : '105' });
-	await transactions.debt.update(app, 2, { debtType : true, srcAmount : '10' });
-	await transactions.debt.update(app, 3, { debtType : false, acc : 2, srcAmount : '200.0202' });
-	await transactions.debt.update(app, 4, { acc : null, srcAmount : '200' });
-	await transactions.debt.update(app, 5, { srcAmount : '1001' });
-}
-
-
-async function runDeleteExpenseTests(app)
-{
-	app.view.setBlock('Delete expense transactions', 2);
-
-	await transactions.del(app, App.EXPENSE, [0]);
-	await transactions.del(app, App.EXPENSE, [0, 1]);
-}
-
-
-async function runDeleteIncomeTests(app)
-{
-	app.view.setBlock('Delete income transactions', 2);
-
-	await transactions.del(app, App.INCOME, [0]);
-	await transactions.del(app, App.INCOME, [0, 1, 2]);
-}
-
-
-async function runDeleteTransferTests(app)
-{
-	app.view.setBlock('Delete transfer transactions', 2);
-
-	await transactions.del(app, App.TRANSFER, [1]);
-	await transactions.del(app, App.TRANSFER, [0, 2]);
-}
-
-
-async function runDeleteDebtTests(app)
-{
-	app.view.setBlock('Delete debt transactions', 2);
-
-	await transactions.del(app, App.DEBT, [0]);
-	await transactions.del(app, App.DEBT, [0, 1]);
-}
-
-
-async function apiTests(app)
-{
-	await runAPI.run(app);
-}
-
-
-async function transactionsListTests(app)
-{
-	await runTransList.run(app);
-}
-
 
 
 let App = new Application;
