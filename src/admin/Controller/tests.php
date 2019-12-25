226ea770205d0646941372f611187133804a91a2
Ilya Makarov <feesler@gmail.com>
2019-12-25 03:57:55 +0300

    * Update build test scripts

--- src/admin/Controller/tests.php
+++ src/admin/Controller/tests.php
@@ -20,7 +20,7 @@ class TestsAdminController extends Controller
 		$this->cssAdmin[] = "tests.css";
 		$this->buildCSS();
 
-		$this->jsAdminModule[] = "tests/browser.js";
+		$this->jsAdminModule[] = "tests/index.js";
 
 		include("./view/templates/tests.tpl");
 	}
