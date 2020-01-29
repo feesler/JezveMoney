7c755c33a977e4533171dbac0597b7dc2669f03e
Ilya Makarov <feesler@gmail.com>
2020-01-29 13:49:16 +0300

    * Fix CSV export script

--- src/csvexport.php
+++ src/csvexport.php
@@ -41,6 +41,8 @@
 	}
 
 
+	$db = mysqlDB::getInstance();
+
 	$uMod = UserModel::getInstance();
 	$user_id = $uMod->check();
 	if (!$user_id)
@@ -64,7 +66,7 @@
 
 	$condArr = ["user_id=".$user_id, "id=".$checkAccount_id];
 	$qResult = $db->selectQ("*", "accounts", $condArr);
-	while($row = $this->dbObj->fetchRow($qResult))
+	while($row = $db->fetchRow($qResult))
 	{
 		$realBalance[$checkAccount_id] = floatval($row["initbalance"]);
 	}
@@ -86,68 +88,63 @@
 	}
 
 	$qResult = $db->selectQ("*", "transactions", $condArr, NULL, "pos");
-	while($row = $this->dbObj->fetchRow($qResult))
+	while($row = $db->fetchRow($qResult))
 	{
 		$tr_id = intval($row["id"]);
 		$tr_type = intval($row["type"]);
 		$tr_src_id = intval($row["src_id"]);
 		$tr_dest_id = intval($row["dest_id"]);
-		$amount = floatval($row["amount"]);
-		$charge = floatval($row["charge"]);
+		$src_amount = floatval($row["src_amount"]);
+		$dest_amount = floatval($row["dest_amount"]);
+		$src_result = floatval($row["src_result"]);
+		$dest_result = floatval($row["dest_result"]);
 		$comment = $row["comment"];
 		$trdate = $row["date"];
 		$tr_pos = intval($row["pos"]);
 
-		$src_name = $accMod->getName($tr_src_id);
-		$dest_name = $accMod->getName($tr_dest_id);
+		$srcAccount = $accMod->getItem($tr_src_id);
+		$src_name = $srcAccount ? $srcAccount->name : NULL;
 
+		$destAccount = $accMod->getItem($tr_dest_id);
+		$dest_name = $srcAccount ? $srcAccount->name : NULL;
 
 		$resStr .= $tr_id.$delim;
 
+		$realBalance = ($tr_src_id == $checkAccount_id) ? $src_result : $dest_result;
+
 		if ($tr_type == 1)				// expense
 		{
-			$resStr .= "Expense".$delim.csv_numeric(-$amount).$delim.csv_numeric(-$charge);
-
-			$realBalance[$tr_src_id] = round($realBalance[$tr_src_id] - $charge, 2);
+			$resStr .= "Expense".$delim.csv_numeric(-$src_amount).$delim.csv_numeric(-$dest_amount);
 		}
 		else if ($tr_type == 2)			// income
 		{
-			$resStr .= "Income".$delim.csv_numeric($amount).$delim.csv_numeric($charge);
-
-			$realBalance[$tr_dest_id] = round($realBalance[$tr_dest_id] + $charge, 2);
+			$resStr .= "Income".$delim.csv_numeric($src_amount).$delim.csv_numeric($dest_amount);
 		}
 		else if ($checkAccount_id != 0 && $tr_type == 3 && $tr_dest_id == $checkAccount_id)			// transfer to
 		{
-			$resStr .= dquote("Transfer from ".csv_encode($src_name)).$delim.csv_numeric($amount).$delim.csv_numeric($charge);
-
-			$realBalance[$checkAccount_id] = round($realBalance[$checkAccount_id] + $amount, 2);
+			$resStr .= dquote("Transfer from ".csv_encode($src_name)).$delim.csv_numeric($src_amount).$delim.csv_numeric($dest_amount);
 		}
 		else if ($checkAccount_id != 0 && $tr_type == 3 && $tr_src_id == $checkAccount_id)			// transfer from
 		{
-			$resStr .= dquote("Transfer to ".csv_encode($dest_name)).$delim.csv_numeric(-$amount).$delim.csv_numeric(-$charge);
-
-			$realBalance[$checkAccount_id] = round($realBalance[$checkAccount_id] - $charge, 2);
+			$resStr .= dquote("Transfer to ".csv_encode($dest_name)).$delim.csv_numeric(-$src_amount).$delim.csv_numeric(-$dest_amount);
 		}
 		else if ($tr_type == 4)
 		{
 			if ($tr_src_id == $checkAccount_id)	// I give
 			{
-				$resStr .= dquote(csv_encode("Debt: out")).$delim.csv_numeric(-$charge).$delim.csv_numeric(-$charge);
+				$resStr .= dquote(csv_encode("Debt: out")).$delim.csv_numeric(-$dest_amount).$delim.csv_numeric(-$dest_amount);
 			}
 			else if ($tr_dest_id == $checkAccount_id)	// I take
 			{
-				$resStr .= dquote(csv_encode("Debt: in")).$delim.csv_numeric($amount).$delim.csv_numeric($amount);
+				$resStr .= dquote(csv_encode("Debt: in")).$delim.csv_numeric($src_amount).$delim.csv_numeric($src_amount);
 			}
-
-			$realBalance[$tr_src_id] = round($realBalance[$tr_src_id] - $charge, 2);
-			$realBalance[$tr_dest_id] = round($realBalance[$tr_dest_id] + $amount, 2);
 		}
 
 		$resStr .= $delim;
 		if ($comment != "")
 			$resStr .= dquote(csv_encode($comment));
 
-		$resStr .= $delim.csv_numeric($realBalance[$checkAccount_id]).$delim.dquote(date("d.m.Y", strtotime($trdate)));
+		$resStr .= $delim.csv_numeric($realBalance).$delim.dquote(date("d.m.Y", strtotime($trdate)));
 
 		$resStr .= "\r\n";
 	}
