2c030e85ed0f7bf638e678af6ca3ababcd5ad95b
Ilya Makarov <feesler@gmail.com>
2020-02-25 05:33:01 +0300

    * Rename htmlConvert() to e()

--- src/system/common.php
+++ src/system/common.php
@@ -76,20 +76,13 @@
 
 
 	// Convert string to be compatible with HTML
-	function htmlConvert($str, $lineEnd = FALSE)
+	function e($str, $lineEnd = FALSE)
 	{
 		$str = htmlentities($str, ENT_QUOTES, "UTF-8");
 		if ($lineEnd)
 			$str = str_replace(["\r\n", "\r", "\n"], "<br>", $str);
 
 		return $str;
-	}
-
-
-	// Short alias for htmlConvert
-	function e($str)
-	{
-		return htmlConvert($str);
 	}
 
 
