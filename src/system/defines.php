ca73393eaea9aceed730bdccb85a6e19c0b41a1f
Ilya Makarov <feesler@gmail.com>
2022-06-20 00:54:55 +0300

    * Update version to 1.14.2

--- src/system/defines.php
+++ src/system/defines.php
@@ -15,7 +15,7 @@ UPLOAD_PATH - path to the uploads directory
 LOGS_PATH - path to the logs directory
 */
 
-define("APP_VERSION", "1.14.1");
+define("APP_VERSION", "1.14.2");
 define("APP_PROTOCOL", "https://");
 
 define("PROD_HOST", "jezvemoney.ru");
