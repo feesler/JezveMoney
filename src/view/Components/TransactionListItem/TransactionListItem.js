9a004b0e457038473ef0bcb209cfdb9c0608f2d1
Ilya Makarov <feesler@gmail.com>
2022-09-16 08:46:29 +0300

    * Split accounts, amount and result fields of TransactionListItem component in details mode to source and destination parts

--- src/view/Components/TransactionListItem/TransactionListItem.js
+++ src/view/Components/TransactionListItem/TransactionListItem.js
@@ -1,8 +1,4 @@
-import {
-    ce,
-    removeChilds,
-    Component,
-} from 'jezvejs';
+import { ce, Component } from 'jezvejs';
 import {
     EXPENSE,
     INCOME,
@@ -14,31 +10,36 @@ import './style.scss';
 
 /** CSS classes */
 export const TRANS_ITEM_CLASS = 'trans-item';
-/* Fields */
-const TITLE_FIELD_CLASS = 'trans-item__title-field';
-const AMOUNT_FIELD_CLASS = 'trans-item__amount-field';
-const BALANCE_FIELD_CLASS = 'trans-item__balance-field';
-const DATE_FIELD_CLASS = 'trans-item__date-field';
-const COMMENT_FIELD_CLASS = 'trans-item__comment-field';
-
 const TITLE_CLASS = 'trans-item__title';
 const AMOUNT_CLASS = 'trans-item__amount';
 const DATE_CLASS = 'trans-item__date';
-const BALANCE_CLASS = 'trans-item__balance';
-const BALANCE_VALUE_CLASS = 'trans-item__balance-value';
 const COMMENT_CLASS = 'trans-item__comment';
-const AMOUNT_RESULT_CLASS = 'trans-item__amount-result';
 const DATE_COMMENT_CLASS = 'trans-item__date-comment';
+/* Details mode */
 const DETAILS_CLASS = 'trans-item_details';
+/* Field groups */
+const ACCOUNTS_GROUP_CLASS = 'trans-item__accounts-group';
+const AMOUNT_GROUP_CLASS = 'trans-item__amount-group';
+const RESULTS_GROUP_CLASS = 'trans-item__results-group';
+const AMOUNT_RESULT_GROUP_CLASS = 'trans-item__amount-result-group';
+/* Fields */
+const TITLE_FIELD_CLASS = 'trans-item__account-field';
+const AMOUNT_FIELD_CLASS = 'trans-item__amount-field';
+const RESULT_FIELD_CLASS = 'trans-item__result-field';
+const DATE_FIELD_CLASS = 'trans-item__date-field';
+const COMMENT_FIELD_CLASS = 'trans-item__comment-field';
+/* Other */
 const SELECTED_CLASS = 'trans-item_selected';
 
 /** Strings */
-const LABEL_SRC_ACCOUNT = 'Source account';
-const LABEL_DEST_ACCOUNT = 'Destination account';
-const LABEL_SRC_PERSON = 'Source person';
-const LABEL_DEST_PERSON = 'Destination person';
+const LABEL_SOURCE = 'Source';
+const LABEL_DESTINATION = 'Destination';
 const LABEL_AMOUNT = 'Amount';
-const LABEL_BALANCE = 'Result balance';
+const LABEL_SRC_AMOUNT = 'Source amount';
+const LABEL_DEST_AMOUNT = 'Destination amount';
+const LABEL_RESULT = 'Result';
+const LABEL_SRC_RESULT = 'Source result';
+const LABEL_DEST_RESULT = 'Destination result';
 const LABEL_DATE = 'Date';
 const LABEL_COMMENT = 'Comment';
 
@@ -86,55 +87,78 @@ export class TransactionListItem extends Component {
     }
 
     initDetails() {
-        this.titleElem = ce('div', { className: TITLE_CLASS });
-        this.srcDestField = Field.create({
-            content: this.titleElem,
+        // Accounts
+        this.sourceField = Field.create({
+            title: LABEL_SOURCE,
             className: TITLE_FIELD_CLASS,
         });
-
-        this.amountElem = ce('div', { className: AMOUNT_CLASS });
-        this.amountField = Field.create({
-            title: LABEL_AMOUNT,
-            content: this.amountElem,
+        this.destField = Field.create({
+            title: LABEL_DESTINATION,
+            className: TITLE_FIELD_CLASS,
+        });
+        this.sourceDestGroup = ce(
+            'div',
+            { className: ACCOUNTS_GROUP_CLASS },
+            [this.sourceField.elem, this.destField.elem],
+        );
+        // Amount
+        this.srcAmountField = Field.create({
+            title: LABEL_SRC_AMOUNT,
             className: AMOUNT_FIELD_CLASS,
         });
-
-        this.balanceElem = ce('div', { className: BALANCE_CLASS });
-        this.balanceField = Field.create({
-            title: LABEL_BALANCE,
-            content: this.balanceElem,
-            className: BALANCE_FIELD_CLASS,
+        this.destAmountField = Field.create({
+            title: LABEL_DEST_AMOUNT,
+            className: AMOUNT_FIELD_CLASS,
         });
-
-        this.amountResultElem = ce(
+        this.amountGroup = ce(
             'div',
-            { className: AMOUNT_RESULT_CLASS },
-            [this.amountField.elem, this.balanceField.elem],
+            { className: AMOUNT_GROUP_CLASS },
+            [this.srcAmountField.elem, this.destAmountField.elem],
+        );
+        // Result balance
+        this.srcResultField = Field.create({
+            title: LABEL_SRC_RESULT,
+            className: RESULT_FIELD_CLASS,
+        });
+        this.destResultField = Field.create({
+            title: LABEL_DEST_RESULT,
+            className: RESULT_FIELD_CLASS,
+        });
+        this.resultsGroup = ce(
+            'div',
+            { className: RESULTS_GROUP_CLASS },
+            [this.srcResultField.elem, this.destResultField.elem],
         );
 
+        this.amountResultGroup = ce(
+            'div',
+            { className: AMOUNT_RESULT_GROUP_CLASS },
+            [this.amountGroup, this.resultsGroup],
+        );
+        // Date
         this.dateElem = ce('div', { className: DATE_CLASS });
         this.dateField = Field.create({
             title: LABEL_DATE,
             content: this.dateElem,
             className: DATE_FIELD_CLASS,
         });
-
+        // Comment
         this.commentElem = ce('div', { className: COMMENT_CLASS });
         this.commentField = Field.create({
             content: this.commentElem,
             className: COMMENT_FIELD_CLASS,
         });
 
-        this.dateCommentElem = ce(
+        this.dateCommentGroup = ce(
             'div',
             { className: DATE_COMMENT_CLASS },
             [this.dateField.elem, this.commentField.elem],
         );
 
         this.elem = ce('div', { className: `${TRANS_ITEM_CLASS} ${DETAILS_CLASS}` }, [
-            this.srcDestField.elem,
-            this.amountResultElem,
-            this.dateCommentElem,
+            this.sourceDestGroup,
+            this.amountResultGroup,
+            this.dateCommentGroup,
         ]);
     }
 
@@ -148,30 +172,18 @@ export class TransactionListItem extends Component {
         return (!!srcAcc && srcAcc.owner_id !== profile.owner_id);
     }
 
-    getTitle(item) {
-        if (item.type === EXPENSE) {
-            return LABEL_SRC_ACCOUNT;
+    getAccountOrPerson(accountId) {
+        const { profile, accounts, persons } = window.app.model;
+        const account = accounts.getItem(accountId);
+        if (!account) {
+            return null;
         }
-        if (item.type === INCOME) {
-            return LABEL_DEST_ACCOUNT;
-        }
-        if (item.type === TRANSFER) {
-            return `${LABEL_SRC_ACCOUNT} → ${LABEL_DEST_ACCOUNT}`;
-        }
-        if (item.type !== DEBT) {
-            throw new Error('Invalid transaction type');
+        if (account.owner_id === profile.owner_id) {
+            return account.name;
         }
 
-        const debtType = this.getDebtType(item);
-        const noAccount = ((debtType) ? item.dest_id : item.src_id) === 0;
-        if (noAccount) {
-            return (debtType) ? LABEL_SRC_PERSON : LABEL_DEST_PERSON;
-        }
-
-        return ((debtType)
-            ? `${LABEL_SRC_PERSON} → ${LABEL_DEST_ACCOUNT}`
-            : `${LABEL_SRC_ACCOUNT} → ${LABEL_DEST_PERSON}`
-        );
+        const person = persons.getItem(account.owner_id);
+        return person.name;
     }
 
     formatAccounts(item) {
@@ -250,22 +262,82 @@ export class TransactionListItem extends Component {
             : `${sign}${srcAmountFmt}`;
     }
 
-    formatResults(item) {
-        if (!item) {
-            throw new Error('Invalid transaction');
-        }
+    renderClassic(state) {
+        const { item } = state;
 
-        const currencyModel = window.app.model.currency;
-        const res = [];
+        const accountTitle = this.formatAccounts(item);
+        this.titleElem.textContent = accountTitle;
+        this.titleElem.setAttribute('title', accountTitle);
 
-        if (item.src_id) {
-            res.push(currencyModel.formatCurrency(item.src_result, item.src_curr));
-        }
-        if (item.dest_id) {
-            res.push(currencyModel.formatCurrency(item.dest_result, item.dest_curr));
-        }
+        this.amountElem.textContent = this.formatAmount(item);
 
-        return res;
+        this.dateElem.textContent = item.date;
+
+        this.commentElem.textContent = item.comment;
+        this.commentElem.setAttribute('title', item.comment);
+    }
+
+    renderDetails(state) {
+        const { item } = state;
+        const { currency } = window.app.model;
+
+        // Source
+        const showSource = (item.src_id !== 0);
+        if (showSource) {
+            const sourceContent = this.getAccountOrPerson(item.src_id);
+            this.sourceField.setContent(sourceContent);
+        }
+        this.sourceField.show(showSource);
+
+        // Destination
+        const showDest = (item.dest_id !== 0);
+        if (showDest) {
+            const destContent = this.getAccountOrPerson(item.dest_id);
+            this.destField.setContent(destContent);
+        }
+        this.destField.show(showDest);
+
+        // Amount
+        const isDiff = (item.src_curr !== item.dest_curr);
+        const srcAmountLabel = (isDiff) ? LABEL_SRC_AMOUNT : LABEL_AMOUNT;
+        this.srcAmountField.setTitle(srcAmountLabel);
+        if (isDiff) {
+            const srcAmountFmt = currency.formatCurrency(item.src_amount, item.src_curr);
+            this.srcAmountField.setContent(srcAmountFmt);
+            const destAmountFmt = currency.formatCurrency(item.dest_amount, item.dest_curr);
+            this.destAmountField.setContent(destAmountFmt);
+        } else {
+            const amountFmt = this.formatAmount(item);
+            this.srcAmountField.setContent(amountFmt);
+        }
+        this.destAmountField.show(isDiff);
+
+        // Source result
+        if (showSource) {
+            const srcResultLabel = (showDest) ? LABEL_SRC_RESULT : LABEL_RESULT;
+            this.srcResultField.setTitle(srcResultLabel);
+            const srcResult = currency.formatCurrency(item.src_result, item.src_curr);
+            this.srcResultField.setContent(srcResult);
+        }
+        this.srcResultField.show(showSource);
+
+        // Destination result
+        if (showDest) {
+            const destResultLabel = (showSource) ? LABEL_DEST_RESULT : LABEL_RESULT;
+            this.destResultField.setTitle(destResultLabel);
+            const destResult = currency.formatCurrency(item.dest_result, item.dest_curr);
+            this.destResultField.setContent(destResult);
+        }
+        this.destResultField.show(showDest);
+
+        // Date
+        this.dateField.setContent(item.date);
+
+        // Comment
+        const commentLabel = (item.comment.length > 0) ? LABEL_COMMENT : null;
+        this.commentField.setTitle(commentLabel);
+        this.commentElem.textContent = item.comment;
+        this.commentElem.setAttribute('title', item.comment);
     }
 
     render(state) {
@@ -279,35 +351,13 @@ export class TransactionListItem extends Component {
         }
 
         this.elem.setAttribute('data-id', item.id);
+        this.elem.setAttribute('data-type', item.type);
 
         if (state.mode === 'details') {
-            const scrDestTitle = this.getTitle(item);
-            this.srcDestField.setTitle(scrDestTitle);
+            this.renderDetails(state);
+        } else {
+            this.renderClassic(state);
         }
-
-        const accountTitle = this.formatAccounts(item);
-        this.titleElem.textContent = accountTitle;
-        this.titleElem.setAttribute('title', accountTitle);
-
-        this.amountElem.textContent = this.formatAmount(item);
-
-        if (state.mode === 'details') {
-            const results = this.formatResults(item);
-            const elems = results.map((res) => (
-                ce('span', { className: BALANCE_VALUE_CLASS, textContent: res })
-            ));
-            removeChilds(this.balanceElem);
-            this.balanceElem.append(...elems);
-        }
-
-        this.dateElem.textContent = item.date;
-
-        if (state.mode === 'details') {
-            const commentLabel = (item.comment.length > 0) ? LABEL_COMMENT : null;
-            this.commentField.setTitle(commentLabel);
-        }
-        this.commentElem.textContent = item.comment;
-        this.commentElem.setAttribute('title', item.comment);
 
         if (state.selected) {
             this.elem.classList.add(SELECTED_CLASS);
