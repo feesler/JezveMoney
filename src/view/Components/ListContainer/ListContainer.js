668c8dec1989723fff611d89a373523f7720910e
Ilya Makarov <feesler@gmail.com>
2023-01-30 07:53:03 +0300

    * Fixed item position at 'onSort' callback of SortableListContainer

--- src/view/Components/ListContainer/ListContainer.js
+++ src/view/Components/ListContainer/ListContainer.js
@@ -98,7 +98,7 @@ export class ListContainer extends Component {
     }
 
     /**
-     * Return item id from specified item element
+     * Returns item id from specified item element
      * @param {Element} elem - target list item element
      */
     itemIdFromElem(elem) {
@@ -108,6 +108,14 @@ export class ListContainer extends Component {
         }
 
         return parseInt(listItemElem.dataset.id, 10);
+    }
+
+    /**
+     * Returns item fom specified list element
+     * @param {Element} elem - target list item element
+     */
+    itemFromElem(elem) {
+        return this.getItemById(this.itemIdFromElem(elem));
     }
 
     /**
