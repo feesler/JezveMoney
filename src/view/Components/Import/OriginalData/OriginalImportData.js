fffd65ec8c59aa8df89d50808929d53ec36bdbef
Ilya Makarov <feesler@gmail.com>
2023-01-31 22:00:27 +0300

    * Refactor layout of OriginalImportData component

--- src/view/Components/Import/OriginalData/OriginalImportData.js
+++ src/view/Components/Import/OriginalData/OriginalImportData.js
@@ -5,6 +5,7 @@ import './style.scss';
 /** CSS classes */
 const CONTAINER_CLASS = 'orig-data';
 const TABLE_CLASS = 'orig-data-table';
+const GROUP_CLASS = 'column-group';
 const COLUMN_CLASS = 'column';
 const COLUMN_HEADER_CLASS = 'column__header';
 const COLUMN_DATA_CLASS = 'column__data';
@@ -35,6 +36,19 @@ export class OriginalImportData extends Component {
         this.render();
     }
 
+    renderGroup(children, className = null) {
+        const elemClasses = [GROUP_CLASS];
+
+        if (typeof className === 'string' && className.length > 0) {
+            elemClasses.push(className);
+        }
+
+        return createElement('div', {
+            props: { className: elemClasses.join(' ') },
+            children,
+        });
+    }
+
     renderColumn(title, value, className = null) {
         const elemClasses = [COLUMN_CLASS];
 
@@ -60,21 +74,33 @@ export class OriginalImportData extends Component {
         const templateName = (template) ? template.name : '';
 
         const dataTable = [
-            [__('IMPORT_MAIN_ACCOUNT'), this.props.origAccount.name],
-            [__('TEMPLATE'), templateName],
-            [__('COLUMN_DATE'), window.app.formatDate(new Date(this.props.date))],
-            [__('COLUMN_TR_AMOUNT'), this.props.transactionAmount],
-            [__('COLUMN_TR_CURRENCY'), this.props.transactionCurrency],
-            [__('COLUMN_ACCOUNT_AMOUNT'), this.props.accountAmount],
-            [__('COLUMN_ACCOUNT_CURRENCY'), this.props.accountCurrency],
-            [__('COLUMN_COMMENT'), this.props.comment, COMMENT_COLUMN_CLASS],
+            [
+                [__('IMPORT_MAIN_ACCOUNT'), this.props.origAccount.name],
+                [__('TEMPLATE'), templateName],
+            ],
+            [
+                [__('COLUMN_TR_AMOUNT'), this.props.transactionAmount],
+                [__('COLUMN_TR_CURRENCY'), this.props.transactionCurrency],
+            ],
+            [
+                [__('COLUMN_ACCOUNT_AMOUNT'), this.props.accountAmount],
+                [__('COLUMN_ACCOUNT_CURRENCY'), this.props.accountCurrency],
+            ],
+            [
+                [__('COLUMN_DATE'), window.app.formatDate(new Date(this.props.date))],
+            ],
+            [
+                [__('COLUMN_COMMENT'), this.props.comment, COMMENT_COLUMN_CLASS],
+            ],
         ];
 
         this.elem = window.app.createContainer(CONTAINER_CLASS, [
-            createElement('h3', { props: { textContent: __('IMPORT_ORIG_DATA') } }),
+            createElement('header', { props: { textContent: __('IMPORT_ORIG_DATA') } }),
             window.app.createContainer(
                 TABLE_CLASS,
-                dataTable.map((col) => this.renderColumn(...col)),
+                dataTable.map((group) => this.renderGroup(
+                    group.map((col) => this.renderColumn(...col)),
+                )),
             ),
         ]);
     }
