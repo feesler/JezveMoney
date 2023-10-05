1172faf57a0105f69baab7ec785a065e3bd7c725
Ilya Makarov <feesler@gmail.com>
2023-10-05 18:25:33 +0300

    * [NavigationMenu] Inherit component from Menu

--- src/view/Components/Layout/NavigationMenu/NavigationMenu.js
+++ src/view/Components/Layout/NavigationMenu/NavigationMenu.js
@@ -1,46 +1,38 @@
-import {
-    Component,
-    removeChilds,
-    createElement,
-    addChilds,
-} from 'jezvejs';
-import { Button } from 'jezvejs/Button';
+import { getClassName } from 'jezvejs';
+import { Menu } from 'jezvejs/Menu';
 
-import { __, getApplicationURL } from '../../../utils/utils.js';
-import { App } from '../../../Application/App.js';
-
-import { Badge } from '../../Common/Badge/Badge.js';
+import { NavigationMenuListItem } from './components/ListItem/MenuItem.js';
 
 import './NavigationMenu.scss';
 
 /* CSS classes */
 const MENU_CLASS = 'nav-list';
-const ITEM_CLASS = 'nav-item';
-const ITEM_LINK_CLASS = 'nav-item__link';
-const CREATE_BTN_CLASS = 'nav-item__icon-btn';
-const SEPARATOR_CLASS = 'nav-separator';
 
 /**
  * Navigation menu component
  */
-export class NavigationMenu extends Component {
+export class NavigationMenu extends Menu {
     constructor(props) {
-        super(props);
-
-        this.state = {
-            ...this.props,
-        };
-
-        this.init();
-        this.render(this.state);
+        super({
+            ...props,
+            className: getClassName(MENU_CLASS, props.className),
+            focusItemOnHover: false,
+            preventNavigation: false,
+            components: {
+                ...props.components,
+                ListItem: NavigationMenuListItem,
+            },
+        });
     }
 
-    init() {
-        this.elem = createElement('ul', { props: { className: MENU_CLASS } });
+    onFocus() {
+    }
+
+    onItemClick() {
     }
 
     setBadgeByURL(url, badge) {
-        this.setState({
+        this.store.setState({
             ...this.state,
             items: this.state.items.map((item) => (
                 (item.url === url)
@@ -48,64 +40,5 @@ export class NavigationMenu extends Component {
                     : item
             )),
         });
-    }
-
-    renderSeparator() {
-        return createElement('li', { props: { className: SEPARATOR_CLASS } });
-    }
-
-    renderMenuItem(item) {
-        if (!item.loggedOut && !App.isUserLoggedIn()) {
-            return null;
-        }
-
-        if (item.type === 'separator') {
-            return this.renderSeparator();
-        }
-
-        const content = (item.content) ?? createElement('a', {
-            props: {
-                className: ITEM_LINK_CLASS,
-                href: getApplicationURL(item.url),
-                textContent: (
-                    (item.titleToken)
-                        ? __(item.titleToken)
-                        : item.title
-                ),
-            },
-        });
-        const children = [content];
-
-        if (item.badge) {
-            const badge = Badge.create({
-                title: item.badge,
-            });
-            children.push(badge.elem);
-        }
-
-        if (item.createButton) {
-            const createBtn = Button.create({
-                type: 'link',
-                url: getApplicationURL(item.createButton),
-                icon: 'plus-light',
-                className: CREATE_BTN_CLASS,
-            });
-            children.push(createBtn.elem);
-        }
-
-        return createElement('li', {
-            props: { className: ITEM_CLASS },
-            children,
-        });
-    }
-
-    render(state) {
-        if (!state) {
-            throw new Error('invalid state');
-        }
-
-        const elems = state.items.map((item) => this.renderMenuItem(item));
-        removeChilds(this.elem);
-        addChilds(this.elem, elems);
     }
 }
