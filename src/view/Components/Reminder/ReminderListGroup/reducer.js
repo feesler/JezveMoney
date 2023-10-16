dec2fe87b494b2c25affde1d941c5e23609363dc
Ilya Makarov <feesler@gmail.com>
2023-10-16 20:20:02 +0300

    * [ReminderListGroup] Combined reducers from ReminderListView and SelectReminderDialog components

---
+++ src/view/Components/Reminder/ReminderListGroup/reducer.js
@@ -0,0 +1,285 @@
+import { createSlice } from 'jezvejs/Store';
+import {
+    dateStringToTime,
+    reduceDeselectItem,
+    reduceSelectItem,
+    reduceToggleItem,
+    formatDateRange,
+} from '../../../utils/utils.js';
+import { App } from '../../../Application/App.js';
+import { ScheduledTransaction } from '../../../Models/ScheduledTransaction.js';
+import { ReminderList } from '../../../Models/ReminderList.js';
+import { REMINDER_SCHEDULED, REMINDER_UPCOMING } from '../../../Models/Reminder.js';
+
+/** Returns initial state object */
+export const getInitialState = (props = {}) => ({
+    listMode: 'list',
+    ...props,
+    form: {
+        ...(props?.filter ?? {}),
+        ...formatDateRange(props?.filter ?? {}),
+    },
+    upcomingItems: null,
+    loading: false,
+    isLoadingMore: false,
+    renderTime: null,
+});
+
+export const getStateFilter = (state) => (
+    state?.filter?.reminderState ?? REMINDER_SCHEDULED
+);
+
+export const getItemsSource = (state) => (
+    (getStateFilter(state) === REMINDER_UPCOMING)
+        ? (state.upcomingItems ?? [])
+        : App.model.reminders.data
+);
+
+export const createList = (items, state) => {
+    const stateFilter = getStateFilter(state);
+    const { startDate, endDate } = state.filter;
+
+    const res = items.filter((item) => (
+        stateFilter === item?.state
+        && (!startDate || item.date >= startDate)
+        && (!endDate || item.date <= endDate)
+    ));
+
+    return res;
+};
+
+const upcomingId = (item, index) => ({
+    ...item,
+    id: `u${index}`,
+});
+
+// Reducers
+const reduceDeselectAll = (state) => ({
+    ...state,
+    items: state.items.map(reduceDeselectItem),
+});
+
+export const updateList = (state, options = {}) => {
+    const keepSelection = options.keepState && state.listMode === 'select';
+    const selectedBefore = (keepSelection)
+        ? state.items.filter((item) => item.selected).map((item) => item.id)
+        : [];
+
+    const result = state;
+    const stateFilter = getStateFilter(result);
+    const { pagination } = result;
+
+    const itemsSource = getItemsSource(state);
+    let items = createList(itemsSource, state);
+    if (keepSelection) {
+        items = items.map((item) => ({
+            ...item,
+            selected: selectedBefore.includes(item.id),
+        }));
+    }
+
+    const list = ReminderList.create(items);
+    if (stateFilter === REMINDER_UPCOMING) {
+        list.sortByDateAsc();
+    }
+    result.items = list;
+
+    if (stateFilter !== REMINDER_UPCOMING) {
+        pagination.pagesCount = Math.ceil(items.length / pagination.onPage);
+    }
+
+    pagination.page = (pagination.pagesCount > 0)
+        ? Math.min(pagination.pagesCount, pagination.page)
+        : 1;
+    pagination.total = items.length;
+
+    return result;
+};
+
+const slice = createSlice({
+    update: (state) => updateList({ ...state }),
+
+    reset: (_, props) => updateList(getInitialState(props)),
+
+    showDetails: (state) => ({
+        ...state,
+        detailsId: state.contextItem,
+        detailsItem: (state.detailsId === state.contextItem) ? state.detailsItem : null,
+        contextItem: null,
+    }),
+
+    closeDetails: (state) => (
+        (state.detailsId)
+            ? { ...state, detailsId: null }
+            : state
+    ),
+
+    itemDetailsLoaded: (state, item) => (
+        (state.detailsId !== item?.id)
+            ? state
+            : {
+                ...state,
+                detailsItem: new ScheduledTransaction(item),
+            }
+    ),
+
+    showMenu: (state) => (
+        (state.showMenu) ? state : { ...state, showMenu: true, showContextMenu: false }
+    ),
+
+    hideMenu: (state) => (
+        (!state.showMenu) ? state : { ...state, showMenu: false }
+    ),
+
+    showContextMenu: (state, itemId) => (
+        (state.contextItem === itemId && state.showContextMenu)
+            ? state
+            : {
+                ...state,
+                contextItem: itemId,
+                showContextMenu: true,
+                showMenu: false,
+            }
+    ),
+
+    hideContextMenu: (state) => (
+        (state.showContextMenu) ? { ...state, showContextMenu: false } : state
+    ),
+
+    toggleSelectItem: (state, itemId) => ({
+        ...state,
+        items: state.items.map(reduceToggleItem(itemId)),
+    }),
+
+    selectAllItems: (state) => ({
+        ...state,
+        items: state.items.map(reduceSelectItem),
+    }),
+
+    deselectAllItems: (state) => reduceDeselectAll(state),
+
+    changeListMode: (state, listMode) => {
+        if (state.listMode === listMode) {
+            return state;
+        }
+
+        const newState = {
+            ...state,
+            listMode,
+            contextItem: null,
+        };
+
+        return (listMode === 'list') ? reduceDeselectAll(newState) : newState;
+    },
+
+    changeStateFilter: (state, reminderState) => updateList({
+        ...state,
+        form: {
+            ...state.form,
+            reminderState,
+        },
+        filter: {
+            ...state.filter,
+            reminderState,
+        },
+        pagination: {
+            ...state.pagination,
+            page: 1,
+            range: 1,
+        },
+    }),
+
+    showMore: (state) => updateList({
+        ...state,
+        pagination: {
+            ...state.pagination,
+            range: (state.pagination.range ?? 0) + 1,
+        },
+    }, { keepState: true }),
+
+    changePage: (state, page) => ({
+        ...state,
+        pagination: {
+            ...state.pagination,
+            page,
+            range: 1,
+        },
+    }),
+
+    changeDateFilter: (state, data) => updateList({
+        ...state,
+        form: {
+            ...state.form,
+            ...data,
+        },
+        filter: {
+            ...state.filter,
+            startDate: dateStringToTime(data.startDate, { fixShortYear: false }),
+            endDate: dateStringToTime(data.endDate, { fixShortYear: false }),
+        },
+    }),
+
+    clearAllFilters: (state) => updateList({
+        ...state,
+        form: {
+            ...state.form,
+            startDate: null,
+            endDate: null,
+        },
+        filter: {
+            ...state.filter,
+            startDate: null,
+            endDate: null,
+        },
+        pagination: {
+            ...state.pagination,
+            page: 1,
+            range: 1,
+        },
+    }),
+
+    toggleMode: (state) => ({
+        ...state,
+        mode: (state.mode === 'details') ? 'classic' : 'details',
+        contextItem: null,
+        renderTime: Date.now(),
+    }),
+
+    startLoading: (state, isLoadingMore = false) => (
+        (state.loading)
+            ? state
+            : { ...state, isLoadingMore, loading: true }
+    ),
+
+    stopLoading: (state) => (
+        (state.loading)
+            ? { ...state, loading: false, isLoadingMore: false }
+            : state
+    ),
+
+    setRenderTime: (state) => ({
+        ...state,
+        renderTime: Date.now(),
+    }),
+
+    listRequestLoaded: (state, { upcoming, keepState }) => {
+        const isUpcoming = getStateFilter(state) === REMINDER_UPCOMING;
+
+        return updateList({
+            ...state,
+            pagination: (isUpcoming && upcoming?.pagination)
+                ? { ...upcoming.pagination }
+                : state.pagination,
+            filter: (isUpcoming && upcoming?.filter)
+                ? { ...state.filter, ...upcoming.filter }
+                : state.filter,
+            upcomingItems: (isUpcoming && Array.isArray(upcoming?.items))
+                ? upcoming.items.map(upcomingId)
+                : state.upcomingItems,
+            listMode: (keepState) ? state.listMode : 'list',
+            contextItem: null,
+        });
+    },
+});
+
+export const { actions, reducer } = slice;
--- src/view/Components/Reminder/SelectReminderDialog/SelectReminderDialog.js
+++ src/view/Components/Reminder/SelectReminderDialog/SelectReminderDialog.js
@@ -10,21 +10,16 @@ import { createStore } from 'jezvejs/Store';
 import {
     DEFAULT_PAGE_LIMIT,
     __,
-    dateStringToTime,
 } from '../../../utils/utils.js';
-import { API } from '../../../API/index.js';
-import { App } from '../../../Application/App.js';
 
-import { REMINDER_SCHEDULED, REMINDER_UPCOMING } from '../../../Models/Reminder.js';
+import { REMINDER_SCHEDULED } from '../../../Models/Reminder.js';
 
 import { ReminderListGroup } from '../ReminderListGroup/ReminderListGroup.js';
 
 import {
     reducer,
-    actions,
-    getStateFilter,
-    updateList,
     getInitialState,
+    actions,
 } from './reducer.js';
 import './SelectReminderDialog.scss';
 
@@ -65,9 +60,10 @@ export class SelectReminderDialog extends Component {
                 ...defaultProps.pagination,
                 ...(props.pagination ?? {}),
             },
+            listMode: 'singleSelect',
         });
 
-        const initialState = updateList(getInitialState(this.props));
+        const initialState = getInitialState(this.props);
         this.store = createStore(reducer, { initialState });
 
         this.init();
@@ -97,18 +93,15 @@ export class SelectReminderDialog extends Component {
         });
 
         this.listGroup = ReminderListGroup.create({
+            ...this.props,
             filtersId: 'filters',
             stateFilterId: 'stateFilter',
             dateRangeFilterId: 'dateFilter',
             modeSelectorType: 'button',
+            listMode: 'singleSelect',
             showControls: false,
-            onChangeReminderState: (range) => this.onChangeReminderState(range),
-            onChangeDateRange: (range) => this.onChangeDateRange(range),
-            onClearAllFilters: (e) => this.onClearAllFilters(e),
-            onItemClick: (id, e) => this.onItemClick(id, e),
-            onShowMore: (e) => this.showMore(e),
-            onChangePage: (page) => this.onChangePage(page),
-            onChangeMode: (mode) => this.onChangeMode(mode),
+            onChange: (selected) => this.onChange(selected),
+            onUpdate: (state) => this.updateRemindersList(state),
         });
 
         this.dialog = Popup.create({
@@ -126,47 +119,13 @@ export class SelectReminderDialog extends Component {
         this.elem = this.dialog.elem;
 
         this.subscribeToStore(this.store);
-        this.onPostInit();
-    }
-
-    async onPostInit() {
-        const state = this.store.getState();
-        const stateFilter = getStateFilter(state);
-
-        if (stateFilter === REMINDER_UPCOMING && state.upcomingItems === null) {
-            await this.requestUpcoming(this.getUpcomingRequestData());
-        }
-        this.setRenderTime();
-    }
-
-    /** Updates dialog state */
-    update() {
-        this.store.dispatch(actions.update());
-    }
-
-    /** Reset dialog state */
-    reset() {
-        this.store.dispatch(actions.reset(this.props));
-    }
-
-    startLoading(isLoadingMore = false) {
-        this.store.dispatch(actions.startLoading(isLoadingMore));
-    }
-
-    stopLoading() {
-        this.store.dispatch(actions.stopLoading());
-    }
-
-    /** Updates render time */
-    setRenderTime() {
-        this.store.dispatch(actions.setRenderTime());
     }
 
     /**
      * Shows/hides dialog
      */
     show(value = true) {
-        this.update();
+        this.listGroup?.update();
         this.dialog.show(value);
     }
 
@@ -175,115 +134,10 @@ export class SelectReminderDialog extends Component {
         this.dialog.hide();
     }
 
-    /**
-     * Reminder state filter change callback
-     * @param {string} value - selected state types
-     */
-    async onChangeReminderState(value) {
-        const stateFilter = parseInt(value, 10);
-        const state = this.store.getState();
-        const currentStateFilter = getStateFilter(state);
-        if (currentStateFilter === stateFilter) {
-            return;
+    onChange(selected) {
+        if (isFunction(this.props.onChange)) {
+            this.props.onChange(selected);
         }
-
-        this.store.dispatch(actions.changeStateFilter(stateFilter));
-
-        if (stateFilter === REMINDER_UPCOMING) {
-            await this.requestUpcoming(this.getUpcomingRequestData());
-        }
-
-        this.setRenderTime();
-    }
-
-    async showMore() {
-        const state = this.store.getState();
-        const isUpcoming = getStateFilter(state) === REMINDER_UPCOMING;
-
-        if (!isUpcoming) {
-            this.store.dispatch(actions.showMore());
-            this.setRenderTime();
-            return;
-        }
-
-        const { page } = state.pagination;
-        let { range } = state.pagination;
-        if (!range) {
-            range = 1;
-        }
-        range += 1;
-
-        await this.requestUpcoming({
-            ...this.getUpcomingRequestData(),
-            range,
-            page,
-            keepState: true,
-            isLoadingMore: true,
-        });
-
-        this.setRenderTime();
-    }
-
-    onChangePage(page) {
-        this.store.dispatch(actions.changePage(page));
-        this.setRenderTime();
-    }
-
-    /** Date range filter change handler */
-    async onChangeDateRange(range) {
-        const { filter } = this.store.getState();
-        const startDate = filter.startDate ?? null;
-        const endDate = filter.endDate ?? null;
-        const timeData = {
-            startDate: dateStringToTime(range.startDate, { fixShortYear: false }),
-            endDate: dateStringToTime(range.endDate, { fixShortYear: false }),
-        };
-
-        if (startDate === timeData.startDate && endDate === timeData.endDate) {
-            return;
-        }
-
-        this.store.dispatch(actions.changeDateFilter(range));
-        if (getStateFilter(this.store.getState()) === REMINDER_UPCOMING) {
-            await this.requestUpcoming({
-                ...this.getUpcomingRequestData(),
-                range: 1,
-                page: 1,
-                keepState: true,
-            });
-        }
-
-        this.setRenderTime();
-    }
-
-    /**
-     * Clear all filters
-     * @param {Event} e - click event object
-     */
-    async onClearAllFilters(e) {
-        e.preventDefault();
-
-        this.store.dispatch(actions.clearAllFilters());
-        if (getStateFilter(this.store.getState()) === REMINDER_UPCOMING) {
-            await this.requestUpcoming({
-                ...this.getUpcomingRequestData(),
-                range: 1,
-                page: 1,
-                keepState: true,
-            });
-        }
-
-        this.setRenderTime();
-    }
-
-    onChangeMode(mode) {
-        const state = this.store.getState();
-        if (state.mode === mode) {
-            return;
-        }
-
-        this.store.dispatch(actions.toggleMode());
-        this.setRenderTime();
     }
 
     onCancel() {
@@ -292,72 +146,8 @@ export class SelectReminderDialog extends Component {
         }
     }
 
-    onItemClick(id) {
-        if (!isFunction(this.props.onChange)) {
-            return;
-        }
-
-        const strId = id?.toString() ?? null;
-        if (strId === null) {
-            return;
-        }
-
-        const state = this.store.getState();
-        const reminder = state.items.find((item) => item.id?.toString() === strId);
-        if (!reminder) {
-            return;
-        }
-
-        const isUpcoming = (reminder.state === REMINDER_UPCOMING);
-        const selected = {
-            schedule_id: reminder.schedule_id,
-            reminder_date: reminder.date,
-            reminder_id: (isUpcoming) ? null : reminder.id,
-        };
-
-        this.props.onChange(selected);
-    }
-
-    getUpcomingRequestData() {
-        const { pagination, form } = this.store.getState();
-
-        const res = {
-            page: pagination.page,
-            range: pagination.range,
-        };
-
-        if (form.startDate) {
-            res.startDate = dateStringToTime(form.startDate, { fixShortYear: false });
-        }
-        if (form.endDate) {
-            res.endDate = dateStringToTime(form.endDate, { fixShortYear: false });
-        }
-
-        return res;
-    }
-
-    async requestUpcoming(options = {}) {
-        const state = this.store.getState();
-        if (state.loading) {
-            return;
-        }
-
-        const {
-            keepState = false,
-            isLoadingMore = false,
-            ...request
-        } = options;
-
-        this.startLoading(isLoadingMore);
-
-        try {
-            const { data: upcoming } = await API.reminder.upcoming(request);
-            this.store.dispatch(actions.listRequestLoaded({ upcoming, keepState }));
-        } catch (e) {
-            App.createErrorNotification(e.message);
-        }
-
-        this.stopLoading();
+    updateRemindersList(data) {
+        this.store.dispatch(actions.updateRemindersList(data));
     }
 
     renderList(state) {
